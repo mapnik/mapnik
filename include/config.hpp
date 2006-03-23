98763057e653b116fe70aca1ddf7a39c5b5c53a8
Artem Pavlenko <artem@mapnik.org>
2006-03-23 11:54:03 +0000

    added  pragma to disable 4996 warning

--- include/config.hpp
+++ include/config.hpp
@@ -7,6 +7,9 @@
 # define MAPNIK_DECL __declspec (dllexport)
 # pragma warning( disable: 4251 )
 # pragma warning( disable: 4275 )
+# if (_MSC_VER >= 1400) // vc8
+#   pragma warning(disable : 4996) //_CRT_SECURE_NO_DEPRECATE
+# endif
 #else
 # define MAPNIK_DECL 
 #endif
