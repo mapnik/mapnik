53193826a858096c7d719e5a30215ddb595d9863
Artem Pavlenko <artem@mapnik.org>
2006-05-26 13:08:48 +0000

    added MAPNIK_DECL

--- include/image_reader.hpp
+++ include/image_reader.hpp
@@ -1,33 +1,35 @@
 /*****************************************************************************
- * 
- * This file is part of Mapnik (c++ mapping toolkit)
- *
- * Copyright (C) 2006 Artem Pavlenko
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- *
- *****************************************************************************/
+* 
+* This file is part of Mapnik (c++ mapping toolkit)
+*
+* Copyright (C) 2006 Artem Pavlenko
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 2.1 of the License, or (at your option) any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with this library; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*
+*****************************************************************************/
 
 //$Id: image_reader.hpp 39 2005-04-10 20:39:53Z pavlenko $
 
 #ifndef IMAGE_READER_HPP
 #define IMAGE_READER_HPP
 
-#include "image_data.hpp"
 #include <stdexcept>
 #include <string>
+
+#include "image_data.hpp"
+#include "config.hpp"
 
 
 namespace mapnik 
@@ -35,30 +37,30 @@ namespace mapnik
     class ImageReaderException : public std::exception
     {
     private:
-	std::string message_;
+        std::string message_;
     public:
-	ImageReaderException(const std::string& message) 
-	    : message_(message) {}
+        ImageReaderException(const std::string& message) 
+            : message_(message) {}
 
-	~ImageReaderException() throw() {}
+        ~ImageReaderException() throw() {}
 
-	virtual const char* what() const throw()
-	{
-	    return message_.c_str();
-	}
+        virtual const char* what() const throw()
+        {
+            return message_.c_str();
+        }
     };
 
-    struct ImageReader
+    struct MAPNIK_DECL ImageReader
     {
-	virtual unsigned width() const=0;
-	virtual unsigned height() const=0;
-	virtual void read(unsigned x,unsigned y,ImageData32& image)=0;
-	virtual ~ImageReader() {}
+        virtual unsigned width() const=0;
+        virtual unsigned height() const=0;
+        virtual void read(unsigned x,unsigned y,ImageData32& image)=0;
+        virtual ~ImageReader() {}
     };
 
     bool register_image_reader(const std::string& type,ImageReader* (*)(const std::string&));
-    ImageReader* get_image_reader(const std::string& type,const std::string& file);
-    
+    MAPNIK_DECL ImageReader* get_image_reader(const std::string& type,const std::string& file);
+
 }
 
 #endif                                            //IMAGE_READER_HPP
