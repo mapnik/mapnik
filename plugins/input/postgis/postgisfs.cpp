18ef4981401b0add2c6311de86de842dd270ad43
Artem Pavlenko <artem@mapnik.org>
2006-10-09 09:45:45 +0000

    don't rely on feature id column name.

--- plugins/input/postgis/postgisfs.cpp
+++ plugins/input/postgis/postgisfs.cpp
@@ -32,7 +32,7 @@ using boost::bad_lexical_cast;
 using std::string;
 
 postgis_featureset::postgis_featureset(boost::shared_ptr<ResultSet> const& rs,
-				     unsigned num_attrs=0)
+                                       unsigned num_attrs=0)
     : rs_(rs),
       num_attrs_(num_attrs),
       totalGeomSize_(0),
@@ -42,66 +42,63 @@ feature_ptr postgis_featureset::next()
 {
     if (rs_->next())
     { 
-	const char* buf = rs_->getValue(0);
-        int id = int4net(buf);
-	feature_ptr feature(new Feature(id));
-        int size=rs_->getFieldLength(1);
-        const char *data=rs_->getValue(1);
+        feature_ptr feature(new Feature(count_));
+        int size=rs_->getFieldLength(0);
+        const char *data=rs_->getValue(0);
         geometry_ptr geom=geometry_utils::from_wkb(data,size,-1);
-	totalGeomSize_+=size;
+        totalGeomSize_+=size;
 	     
         if (geom)
         {
             feature->set_geometry(geom);
-	    unsigned start=2;
-	    for (unsigned pos=0;pos<num_attrs_;++pos)
-	    {
-		std::string name = rs_->getFieldName(start + pos);
-		const char* buf=rs_->getValue(start + pos);
-		//int field_size = rs_->getFieldLength(start + pos);
-		int oid = rs_->getTypeOID(start + pos);
+            
+            for (unsigned pos=1;pos<num_attrs_;++pos)
+            {
+                std::string name = rs_->getFieldName(pos);
+                const char* buf=rs_->getValue(pos);
+                int oid = rs_->getTypeOID(pos);
 		
-		if (oid==23) //int4
-		{
-		    int val = int4net(buf);
-		    boost::put(*feature,name,val);
-		}
-		else if (oid==21) //int2
-		{
-		    int val = int2net(buf);
-		    boost::put(*feature,name,val);
-		}
-		else if (oid == 700) // float4
-		{
-		    float val;
-		    float4net(val,buf);
-		    boost::put(*feature,name,val);
-		}
-		else if (oid == 701) // float8
-		{
-		    double val;
-		    float8net(val,buf);
-		    boost::put(*feature,name,val);
-		}
-		else if (oid==1042 || oid==1043) //bpchar or varchar
-		{
-		    boost::put(*feature,name,buf);
-		}
-		else 
-		{
-		    boost::put(*feature,name,0);
-		}
-	    }
+                if (oid==23) //int4
+                {
+                    int val = int4net(buf);
+                    boost::put(*feature,name,val);
+                }
+                else if (oid==21) //int2
+                {
+                    int val = int2net(buf);
+                    boost::put(*feature,name,val);
+                }
+                else if (oid == 700) // float4
+                {
+                    float val;
+                    float4net(val,buf);
+                    boost::put(*feature,name,val);
+                }
+                else if (oid == 701) // float8
+                {
+                    double val;
+                    float8net(val,buf);
+                    boost::put(*feature,name,val);
+                }
+                else if (oid==1042 || oid==1043) //bpchar or varchar
+                {
+                    boost::put(*feature,name,buf);
+                }
+                else 
+                {
+                    boost::put(*feature,name,0);
+                }
+            }
             ++count_;
         }
-	return feature;
+        return feature;
     }
     else
     {
         rs_->close();
         std::clog << "totalGeomSize="<<totalGeomSize_<<" bytes"<<std::endl;
         std::clog << "count="<<count_<<std::endl;
-	return feature_ptr();
+        return feature_ptr();
     }
 }
 
