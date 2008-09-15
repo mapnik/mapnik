beec0d6a03f7b3939286b3b219a5b1942739ab1b
Artem Pavlenko <artem@mapnik.org>
2008-09-15 08:46:18 +0000

    use text_symbolizer,symbolizer_with_image as bases

--- bindings/python/mapnik_shield_symbolizer.cpp
+++ bindings/python/mapnik_shield_symbolizer.cpp
@@ -30,8 +30,9 @@ void export_shield_symbolizer()
     using namespace boost::python;
     using mapnik::shield_symbolizer;
     using mapnik::text_symbolizer;
+    using mapnik::symbolizer_with_image;
 
-    class_< shield_symbolizer, bases<text_symbolizer> >("ShieldSymbolizer",
+    class_< shield_symbolizer, bases<text_symbolizer,symbolizer_with_image> >("ShieldSymbolizer",
                              init< std::string const&, std::string const&, unsigned, mapnik::Color const&,
                                                         std::string const&, std::string const&,unsigned,unsigned>("TODO"))
         ;
