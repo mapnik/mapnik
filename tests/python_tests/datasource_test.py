00b2dbb26f4fe32d8d4eec50bd11c709a36c7259
Dane Springmeyer <dane@dbsgeo.com>
2009-05-01 01:42:58 +0000

    add more tests for r1121

--- tests/python_tests/datasource_test.py
+++ tests/python_tests/datasource_test.py
@@ -15,3 +15,33 @@ def test_field_listing():
     lyr.datasource = mapnik.Shapefile(file='../data/shp/poly.shp')
     fields = lyr.datasource.fields()
     eq_(fields, ['AREA', 'EAS_ID', 'PRFEDEA'])
+
+def test_total_feature_count():
+    lyr = mapnik.Layer('test')
+    lyr.datasource = mapnik.Shapefile(file='../data/shp/poly.shp')
+    features = lyr.datasource.all_features()
+    num_feats = len(features)
+    eq_(num_feats, 10)
+
+def test_feature_envelope():
+    lyr = mapnik.Layer('test')
+    lyr.datasource = mapnik.Shapefile(file='../data/shp/poly.shp')
+    features = lyr.datasource.all_features()
+    for feat in features:
+        env = feat.envelope()
+        contains = lyr.envelope().contains(env)
+        eq_(contains, True)
+        intersects = lyr.envelope().contains(env)
+        eq_(intersects, True)
+
+def test_feature_attributes():
+    lyr = mapnik.Layer('test')
+    lyr.datasource = mapnik.Shapefile(file='../data/shp/poly.shp')
+    features = lyr.datasource.all_features()
+    feat = features[0]
+    attrs = {'PRFEDEA': u'35043411', 'EAS_ID': 168, 'AREA': 215229.266}
+    eq_(feat.attributes, attrs)
+    fld_name = 'AREA'
+    fld_idx = lyr.datasource.fields().index(fld_name)
+    eq_(lyr.datasource.field_types()[fld_idx],type(feat.attributes[fld_name]))
+    
