d1dbf89961dae1011f5b998a85c9d74fe278bea0
Dane Springmeyer <dane@dbsgeo.com>
2009-12-15 19:09:30 +0000

    disable the wall clock timer as it is not portable code and we can't release at this time

--- include/mapnik/feature_style_processor.hpp
+++ include/mapnik/feature_style_processor.hpp
@@ -34,9 +34,13 @@
 #include <mapnik/utils.hpp>
 #include <mapnik/projection.hpp>
 #include <mapnik/scale_denominator.hpp>
+
+/*
 #ifdef MAPNIK_DEBUG
 #include <mapnik/wall_clock_timer.hpp>
 #endif
+*/
+
 //stl
 #include <vector>
 
@@ -70,9 +74,11 @@ namespace mapnik
 	 
          void apply()
          {
+/*
 #ifdef MAPNIK_DEBUG           
             mapnik::wall_clock_progress_timer t(std::clog, "map rendering took: ");
 #endif          
+*/
             Processor & p = static_cast<Processor&>(*this);
             p.start_map_processing(m_);
                        
@@ -106,9 +112,11 @@ namespace mapnik
          void apply_to_layer(Layer const& lay, Processor & p, 
                              projection const& proj0,double scale_denom)
          {
+/*
 #ifdef MAPNIK_DEBUG
-   wall_clock_progress_timer timer(clog, "end layer rendering: ");
+            wall_clock_progress_timer timer(clog, "end layer rendering: ");
 #endif
+*/
             p.start_layer_processing(lay);
             boost::shared_ptr<datasource> ds=lay.datasource();
             if (ds)
