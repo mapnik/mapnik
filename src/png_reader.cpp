d0b1e62c2d913f120068e1ef2f608879b618c6ae
Artem Pavlenko <artem@mapnik.org>
2010-06-14 13:18:09 +0000

    + removed deprecated direct access to io_ptr

--- src/png_reader.cpp
+++ src/png_reader.cpp
@@ -82,7 +82,7 @@ png_read_data(png_structp png_ptr, png_bytep data, png_size_t length)
 {
     png_size_t check;
     check = (png_size_t)fread(data, (png_size_t)1, length,
-                              (FILE *)png_ptr->io_ptr);
+                              (FILE *)png_get_io_ptr(png_ptr));
 
     if (check != length)
     {
