e7533dd125fe70bbe0d52a605c606042f3785276
Dane Springmeyer <dane@dbsgeo.com>
2011-06-04 00:25:43 +0000

    reflect mapnik.Grid in python

---
+++ bindings/python/mapnik_grid.cpp
@@ -0,0 +1,68 @@
+/*****************************************************************************
+ * 
+ * This file is part of Mapnik (c++ mapping toolkit)
+ *
+ * Copyright (C) 2011 Artem Pavlenko
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ *****************************************************************************/
+//$Id$
+
+// boost
+#include <boost/python.hpp>
+#include <boost/python/module.hpp>
+#include <boost/python/def.hpp>
+
+// mapnik
+#include <mapnik/grid/grid.hpp>
+#include "python_grid_utils.hpp"
+
+using namespace boost::python;
+
+// help compiler see template definitions
+static dict (*encode)( mapnik::grid const&, std::string, bool, unsigned int) = mapnik::grid_encode;
+
+void export_grid()
+{
+    class_<mapnik::grid,boost::shared_ptr<mapnik::grid> >(
+            "Grid",
+            "This class represents a feature hitgrid.",
+            init<int,int,std::string,unsigned>(
+              ( arg("width"),arg("height"),arg("key")="__id__",arg("resolution")=1 ),
+            "Create a mapnik.Grid object\n"
+            ))
+        .def("width",&mapnik::grid::width)
+        .def("height",&mapnik::grid::height)
+        .def("view",&mapnik::grid::get_view)
+        .def("encode",encode,
+            ( arg("encoding")="utf",arg("add_features")=true,arg("resolution")=4 ),
+            "Encode the grid as as optimized json\n"
+            )
+        .add_property("key",
+            make_function(&mapnik::grid::get_key,return_value_policy<copy_const_reference>()),
+            &mapnik::grid::set_key,
+            "Get/Set key to be used as unique indentifier for features\n"
+            "The value should either be __id__ to refer to the feature.id()\n"
+            "or some globally unique integer or string attribute field\n"
+         )
+/*
+        // TODO - will require json generator
+        .def("save", save_to_file1)
+        .def("save", save_to_file2)
+*/
+        ;
+
+}
