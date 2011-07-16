08f1a71e4bed44c3453375892c034157af48719f
Hermann Kraus <herm@scribus.info>
2011-07-16 12:40:33 +0000

    Fix default value.

--- src/metawriter.cpp
+++ src/metawriter.cpp
@@ -96,7 +96,7 @@ metawriter_json_stream::~metawriter_json_stream()
 
 metawriter_json_stream::metawriter_json_stream(metawriter_properties dflt_properties)
     : metawriter(dflt_properties), count_(-1), output_empty_(true),
-      trans_(0), output_srs_("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"), f_(0)
+      trans_(0), output_srs_("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"), f_(0), pixel_coordinates_(false)
 {
 }
 
