09df0f5ef152c2788cc44bdb8b7379ea1a06819d
Dane Springmeyer <dane@dbsgeo.com>
2011-08-23 15:22:32 +0000

    osx: updates to installer scripts

--- osx/installer/scripts/setup.sh
+++ osx/installer/scripts/setup.sh
@@ -1,5 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
+# TODO
+# - find safer way to do this: http://trac.mapnik.org/ticket/776
+# - check for ~/.profile so that adding a new bash_profile does not override
+
+# backup bash_profile
+cp ~/.bash_profile ~/.bash_profile.mapnik.backup
+
+# add installer Programs to PATH
+# by appending new entries to bash_profile
 echo '' >> ~/.bash_profile
 echo '# Settings for Mapnik.framework Installer to enable Mapnik programs and python bindings' >> ~/.bash_profile
 echo 'export PATH=/Library/Frameworks/Mapnik.framework/Programs:$PATH' >> ~/.bash_profile
--- osx/scripts/fetch_py_releases.sh
+++ osx/scripts/fetch_py_releases.sh
@@ -8,14 +8,11 @@ wget http://www.python.org/ftp/python/2.5.4/python-2.5.4-macosx.dmg
 wget http://www.python.org/ftp/python/2.6.6/python-2.6.6-macosx10.3.dmg
 
 # 2.7
-wget http://www.python.org/ftp/python/2.7.1/python-2.7.1-macosx10.6.dmg
-#wget http://www.python.org/ftp/python/2.7/python-2.7-macosx10.5.dmg
+wget http://www.python.org/ftp/python/2.7.2/python-2.7.2-macosx10.6.dmg
 
-# 3.1.3 32 bit
-wget http://www.python.org/ftp/python/3.1.3/python-3.1.3-macosx10.3.dmg
-
-# 3.1.2 sources
-wget http://www.python.org/ftp/python/3.1.2/Python-3.1.2.tar.bz2
+# 3.1.x does not provide 64 bit versions for some reasons, so skip...
+#wget http://www.python.org/ftp/python/3.1.3/python-3.1.3-macosx10.3.dmg
+#wget http://www.python.org/ftp/python/3.1.2/Python-3.1.2.tar.bz2
 
 # 3.2
-wget http://www.python.org/ftp/python/3.2/python-3.2-macosx10.6.dmg
+wget http://www.python.org/ftp/python/3.2.1/python-3.2.1-macosx10.6.dmg
