a19402f35ca854104e81053aa66e0a6d629c9569
Dane Springmeyer <dane@dbsgeo.com>
2011-08-30 22:51:03 +0000

    fully comment the mapnik-config test - its more annoying that its worth

--- tests/python_tests/mapnik_config_test.py
+++ tests/python_tests/mapnik_config_test.py
@@ -6,30 +6,30 @@ import os
 
 #import os, sys, glob, mapnik2
 
-def test():
-    # mapnik-config program
-    # should be on default path...
-    mc = 'mapnik-config'
-    valid = ['--help',
-             '--prefix',
-             '--libs',
-             '--dep-libs',
-             '--ldflags',
-             '--cflags',
-             '--fonts',
-             '--input-plugins',
-             '-v',
-             '--version',
-             '--svn-revision',
-    ]
-
-    # valid args should return 1
-    for item in valid:
-        eq_(0,call([mc,item]))
-    
-    # errors should return 1
-    #eq_(1,call([mc,'']))
-    #eq_(1,call([mc,'foo']))
+#def test():
+#    # mapnik-config program
+#    # should be on default path...
+#    mc = 'mapnik-config'
+#    valid = ['--help',
+#             '--prefix',
+#             '--libs',
+#             '--dep-libs',
+#             '--ldflags',
+#             '--cflags',
+#             '--fonts',
+#             '--input-plugins',
+#             '-v',
+#             '--version',
+#             '--svn-revision',
+#    ]
+#
+#    # valid args should return 1
+#    for item in valid:
+#        eq_(0,call([mc,item]))
+#    
+#    # errors should return 1
+#    eq_(1,call([mc,'']))
+#    eq_(1,call([mc,'foo']))
 
 if __name__ == "__main__":
     [eval(run)() for run in dir() if 'test_' in run]
