4a2151acec5585ba0b8485fe3412923b807ade47
kunitoki <kunitoki@gmail.com>
2011-10-23 15:23:14 +0200

    - fix copyright repeats

--- src/shield_symbolizer.cpp
+++ src/shield_symbolizer.cpp
@@ -3,7 +3,6 @@
  * This file is part of Mapnik (c++ mapping toolkit)
  *
  * Copyright (C) 2011 Artem Pavlenko
- * Copyright (C) 2011 Artem Pavlenko
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
