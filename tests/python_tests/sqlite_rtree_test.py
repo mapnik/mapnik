0999523b87e2b74b647fcef925964e4419dca2ac
Dane Springmeyer <dane@dbsgeo.com>
2011-11-19 11:39:59 -0800

    tests: make the sqlite rtree test more flexible

--- tests/python_tests/sqlite_rtree_test.py
+++ tests/python_tests/sqlite_rtree_test.py
@@ -14,12 +14,12 @@ def setup():
     os.chdir(execution_path('.'))
 
 NUM_THREADS = 10
-
 TOTAL = 245
 DB = '../data/sqlite/world.sqlite'
+TABLE= 'world_merc'
 
 def create_ds():
-    ds = mapnik2.SQLite(file=DB,table='world_merc')
+    ds = mapnik2.SQLite(file=DB,table=TABLE)
     fs = ds.all_features()
 
 if 'sqlite' in mapnik2.DatasourceCache.instance().plugin_names():
@@ -42,19 +42,30 @@ if 'sqlite' in mapnik2.DatasourceCache.instance().plugin_names():
         eq_(os.path.exists(index),True)
         conn = sqlite3.connect(index)
         cur = conn.cursor()
-        cur.execute("Select count(*) from idx_world_merc_GEOMETRY")
+        cur.execute("Select count(*) from idx_%s_GEOMETRY" % TABLE.replace("'",""))
         conn.commit()
         eq_(cur.fetchone()[0],TOTAL)
         cur.close()
 
-        ds = mapnik2.SQLite(file=DB,table='world_merc')
+        ds = mapnik2.SQLite(file=DB,table=TABLE)
         fs = ds.all_features()
         eq_(len(fs),TOTAL)
         os.unlink(index)
-        ds = mapnik2.SQLite(file=DB,table='world_merc',use_spatial_index=False)
+        ds = mapnik2.SQLite(file=DB,table=TABLE,use_spatial_index=False)
         fs = ds.all_features()
         eq_(len(fs),TOTAL)
         eq_(os.path.exists(index),False)
+
+        ds = mapnik2.SQLite(file=DB,table=TABLE,use_spatial_index=True)
+        fs = ds.all_features()
+        for feat in fs:
+            query = mapnik2.Query(feat.envelope())
+            selected = ds.features(query)
+            eq_(len(selected.features)>=1,True)
+
+        eq_(os.path.exists(index),True)
+        os.unlink(index)
+
 
 if __name__ == "__main__":
     setup()
