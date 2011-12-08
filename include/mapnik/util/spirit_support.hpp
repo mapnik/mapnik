ee404ff8c4e6e66ab5e2d27ef3b4048c25b13261
Artem Pavlenko <artem@mapnik.org>
2011-12-08 15:12:56 +0000

    adapt mapnik::geometry_type to work with boost::spirit

---
+++ include/mapnik/util/spirit_support.hpp
@@ -0,0 +1,70 @@
+/*****************************************************************************
+ * 
+ * This file is part of Mapnik (c++ mapping toolkit)
+ *
+ * Copyright (C) 2011 Artem Pavlenko
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ *****************************************************************************/
+
+//$Id$
+
+#ifndef SPIRIT_SUPPORT_HPP
+#define SPIRIT_SUPPORT_HPP
+
+// mapnik
+#include <mapnik/global.hpp>
+#include <mapnik/geometry.hpp>
+#include <mapnik/util/vertex_iterator.hpp>
+
+// boost
+#include <boost/tuple/tuple.hpp>
+#include <boost/spirit/include/karma.hpp>
+
+namespace boost { namespace spirit { namespace traits {
+
+template <>
+struct is_container<mapnik::geometry_type const> : mpl::true_ {} ;
+
+template <>
+struct container_iterator<mapnik::geometry_type const>
+{
+    typedef mapnik::util::vertex_iterator<double> type;
+};
+
+template <>
+struct begin_container<mapnik::geometry_type const>
+{
+    static mapnik::util::vertex_iterator<double>
+    call (mapnik::geometry_type const& g)
+    {
+        return mapnik::util::vertex_iterator<double>(g.data());
+    }
+};
+
+template <>
+struct end_container<mapnik::geometry_type const>
+{
+    static mapnik::util::vertex_iterator<double>
+    call (mapnik::geometry_type const& g)
+    {
+        return mapnik::util::vertex_iterator<double>();
+    }
+};
+
+}}}
+
+#endif // SPIRIT_SUPPORT_HPP
