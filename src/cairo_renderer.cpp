79af8fdbc958b905fcd39d16a701f1d89c085d9b
Cezary Tarnowski <cezary.tarnowski@tomtom.com>
2011-12-13 15:28:18 +0100

    refactoring following comment on pull request

--- src/cairo_renderer.cpp
+++ src/cairo_renderer.cpp
@@ -780,12 +780,12 @@ void cairo_renderer_base::process(building_symbolizer const& sym,
 
     color const& fill = sym.get_fill();
     double height = 0.0;
-	expression_ptr height_expr = sym.height();
-	if (height_expr)
-	{
-		value_type result = boost::apply_visitor(evaluate<Feature,value_type>(feature), *height_expr);
-		height = 0.7071 * result.to_double();
-	}
+    expression_ptr height_expr = sym.height();
+    if (height_expr)
+    {
+        value_type result = boost::apply_visitor(evaluate<Feature,value_type>(feature), *height_expr);
+        height = 0.7071 * result.to_double();
+    }
 
     for (unsigned i = 0; i < feature.num_geometries(); ++i)
     {
