33bd76f24fe00b96c9452fa818ea22e7505e5c4a
Dane Springmeyer <dane@dbsgeo.com>
2012-01-11 20:02:30 -0800

    add geometry_type to mapnik::layer_descriptor - empty unless set by datasources - refs #701

--- include/mapnik/feature_layer_desc.hpp
+++ include/mapnik/feature_layer_desc.hpp
@@ -39,43 +39,55 @@ class layer_descriptor
 public:
     layer_descriptor(std::string const& name, std::string const& encoding)
         : name_(name),
-          encoding_(encoding) {}
+          encoding_(encoding),
+          geometry_type_("") {}
 
     layer_descriptor(layer_descriptor const& other)
         : name_(other.name_),
           encoding_(other.encoding_),
+          geometry_type_(other.geometry_type_),
           desc_ar_(other.desc_ar_) {}
-        
+
     void set_name(std::string const& name)
     {
-        name_=name;
+        name_ = name;
     }
-         
+
     std::string const& get_name() const
     {
         return name_;
     }
-        
-    void set_encoding(std::string const& encoding) 
+
+    void set_encoding(std::string const& encoding)
     {
-        encoding_=encoding;
+        encoding_ = encoding;
     }
-        
+
     std::string const& get_encoding() const
     {
         return encoding_;
+    }
+
+    void set_geometry_type(std::string const& geometry_type)
+    {
+        geometry_type_ = geometry_type;
+    }
+
+    std::string const& get_geometry_type() const
+    {
+        return geometry_type_;
     }
 
     void add_descriptor(attribute_descriptor const& desc)
     {
         desc_ar_.push_back(desc);
     }
-        
+
     std::vector<attribute_descriptor> const& get_descriptors() const
     {
         return desc_ar_;
-    }   
-    
+    }
+
     std::vector<attribute_descriptor>& get_descriptors()
     {
         return desc_ar_;
@@ -84,22 +96,23 @@ public:
 private:
     std::string name_;
     std::string encoding_;
+    std::string geometry_type_;
     std::vector<attribute_descriptor> desc_ar_;
 };
-    
+
 template <typename charT,typename traits>
 inline std::basic_ostream<charT,traits>&
 operator << (std::basic_ostream<charT,traits>& out,
              layer_descriptor const& ld)
 {
-    out << "name=" << ld.get_name() << "\n";
-    out << "encoding=" << ld.get_encoding() << "\n";
-    std::vector<attribute_descriptor> const& desc_ar=ld.get_descriptors();
-    std::vector<attribute_descriptor>::const_iterator pos=desc_ar.begin();
+    out << "name: " << ld.get_name() << "\n";
+    out << "encoding: " << ld.get_encoding() << "\n";
+    out << "geometry_type: " << ld.get_geometry_type() << "\n";
+    std::vector<attribute_descriptor> const& desc_ar = ld.get_descriptors();
+    std::vector<attribute_descriptor>::const_iterator pos = desc_ar.begin();
     while (pos != desc_ar.end())
     {
         out << *pos++ << "\n";
-            
     }
     return out;
 }
