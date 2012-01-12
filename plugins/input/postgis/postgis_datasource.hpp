95c7e4a076daa20497e7c8cbc31164102e4fc457
Dane Springmeyer <dane@dbsgeo.com>
2012-01-12 14:05:03 -0800

    plugins/
    pass by value

--- plugins/input/postgis/postgis_datasource.hpp
+++ plugins/input/postgis/postgis_datasource.hpp
@@ -89,7 +89,7 @@ public:
     void bind() const;
 private:
     std::string sql_bbox(box2d<double> const& env) const;
-    std::string populate_tokens(const std::string& sql, double const& scale_denom, box2d<double> const& env) const;
+    std::string populate_tokens(const std::string& sql, double scale_denom, box2d<double> const& env) const;
     std::string populate_tokens(const std::string& sql) const;
     static std::string unquote(const std::string& sql);
     boost::shared_ptr<IResultSet> get_resultset(boost::shared_ptr<Connection> const &conn, const std::string &sql) const;
