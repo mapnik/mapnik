b838d17680033a980062feada7096b25d906bce7
Artem Pavlenko <artem@mapnik.org>
2012-01-17 13:32:26 -0500

    remove extra empty lines

--- src/agg/process_text_symbolizer.cpp
+++ src/agg/process_text_symbolizer.cpp
@@ -33,8 +33,6 @@ void agg_renderer<T>::process(text_symbolizer const& sym,
                               Feature const& feature,
                               proj_transform const& prj_trans)
 {
-
-
     // Use a boost::ptr_vector here instread of std::vector?
     std::vector<geometry_type*> geometries_to_process;
     unsigned num_geom = feature.num_geometries();
