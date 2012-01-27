d6b8209a9defb0720ade723e432f15dddd2eb4aa
Hermann Kraus <hermr2d2@gmail.com>
2012-01-27 23:13:07 +0100

    Rewrote text processing to use a tree structure. Reduces the number of classes and simplifies extension.

--- include/mapnik/text_processing.hpp
+++ include/mapnik/text_processing.hpp
@@ -46,8 +46,8 @@ enum text_transform
     CAPITALIZE,
     text_transform_MAX
 };
-
 DEFINE_ENUM( text_transform_e, text_transform );
+
 
 struct char_properties
 {
@@ -79,7 +79,8 @@ public:
     UnicodeString str;
 };
 
-class processed_text
+
+class processed_text : boost::noncopyable
 {
 public:
     processed_text(face_manager<freetype_engine> & font_manager, double scale_factor);
@@ -88,8 +89,8 @@ public:
     unsigned empty() const { return expr_list_.empty(); }
     void clear();
     typedef std::list<processed_expression> expression_list;
-    expression_list::const_iterator begin();
-    expression_list::const_iterator end();
+    expression_list::const_iterator begin() const;
+    expression_list::const_iterator end() const;
     string_info &get_string_info();
 private:
     expression_list expr_list_;
@@ -98,7 +99,86 @@ private:
     string_info info_;
 };
 
-class abstract_token;
+
+namespace formating {
+class node;
+typedef boost::shared_ptr<node> node_ptr;
+class node
+{
+public:
+    virtual ~node() {}
+    virtual void to_xml(boost::property_tree::ptree &xml) const;
+    static node_ptr from_xml(boost::property_tree::ptree const& xml);
+    virtual void apply(char_properties const& p, Feature const& feature, processed_text &output) const = 0;
+    virtual void add_expressions(std::set<expression_ptr> &expressions) const;
+};
+
+class list_node: public node {
+public:
+    list_node() : node(), children_() {}
+    virtual void to_xml(boost::property_tree::ptree &xml) const;
+    virtual void apply(char_properties const& p, Feature const& feature, processed_text &output) const;
+    virtual void add_expressions(std::set<expression_ptr> &expressions) const;
+
+    void push_back(node_ptr n);
+    void set_children(std::vector<node_ptr> const& children);
+    std::vector<node_ptr> const& get_children() const;
+    void clear();
+private:
+    std::vector<node_ptr> children_;
+};
+
+class text_node: public node {
+public:
+    text_node(expression_ptr text): node(), text_(text) {}
+    void to_xml(boost::property_tree::ptree &xml) const;
+    static node_ptr from_xml(boost::property_tree::ptree const& xml);
+    virtual void apply(char_properties const& p, Feature const& feature, processed_text &output) const;
+    virtual void add_expressions(std::set<expression_ptr> &expressions) const;
+
+    void set_text(expression_ptr text);
+    expression_ptr get_text() const;
+private:
+    expression_ptr text_;
+};
+
+class format_node: public node {
+public:
+    format_node();
+    void to_xml(boost::property_tree::ptree &xml) const;
+    static node_ptr from_xml(boost::property_tree::ptree const& xml);
+    virtual void apply(char_properties const& p, Feature const& feature, processed_text &output) const;
+
+    void set_child(node_ptr child);
+    node_ptr get_child() const;
+
+    void set_face_name(boost::optional<std::string> face_name);
+    void set_text_size(boost::optional<unsigned> text_size);
+    void set_character_spacing(boost::optional<unsigned> character_spacing);
+    void set_line_spacing(boost::optional<unsigned> line_spacing);
+    void set_text_opacity(boost::optional<double> opacity);
+    void set_wrap_before(boost::optional<bool> wrap_before);
+    void set_wrap_char(boost::optional<unsigned> wrap_char);
+    void set_text_transform(boost::optional<text_transform_e> text_trans);
+    void set_fill(boost::optional<color> fill);
+    void set_halo_fill(boost::optional<color> halo_fill);
+    void set_halo_radius(boost::optional<double> radius);
+private:
+    boost::optional<std::string> face_name_;
+    boost::optional<unsigned> text_size_;
+    boost::optional<unsigned> character_spacing_;
+    boost::optional<unsigned> line_spacing_;
+    boost::optional<double> text_opacity_;
+    boost::optional<bool> wrap_before_;
+    boost::optional<unsigned> wrap_char_;
+    boost::optional<text_transform_e> text_transform_;
+    boost::optional<color> fill_;
+    boost::optional<color> halo_fill_;
+    boost::optional<double> halo_radius_;
+    node_ptr child_;
+};
+
+} //namespace formating
 
 /** Stores formating information and uses this to produce formated text for a given feature. */
 class text_processor
@@ -113,22 +193,21 @@ public:
     /** Takes a feature and produces formated text as output. 
      * The output object has to be created by the caller and passed in for thread safety.
      */
-    void process(processed_text &output, Feature const& feature);
+    void process(processed_text &output, Feature const& feature) const;
     /** Automatically create processing instructions for a single expression. */
     void set_old_style_expression(expression_ptr expr);
-    /** Add a new formating token. */
-    void push_back(abstract_token *token);
+    /** Sets new format tree. */
+    void set_format_tree(formating::node_ptr tree);
+    /** Get format tree. */
+    formating::node_ptr get_format_tree() const;
     /** Get a list of all expressions used in any placement. This function is used to collect attributes. */
     std::set<expression_ptr> get_all_expressions() const;
     /** Default values for char_properties. */
     char_properties defaults;
-protected:
-    void from_xml_recursive(boost::property_tree::ptree const& pt, std::map<std::string,font_set> const &fontsets);
 private:
-    std::list<abstract_token *> list_;
-    bool clear_on_write; //Clear list once
+    formating::node_ptr tree_;
 };
 
-} /* namespace */
+} /* namespace mapnik*/
 
 #endif
