ad3072300f6de8bebd14d4e541452c4b59f4cbf2
Artem Pavlenko <artem@mapnik.org>
2012-03-05 15:29:21 +0000

    + remove mutable qualifiers

--- plugins/input/osm/osm_datasource.hpp
+++ plugins/input/osm/osm_datasource.hpp
@@ -40,24 +40,22 @@ using mapnik::box2d;
 class osm_datasource : public datasource
 {
 public:
-    osm_datasource(const parameters& params, bool bind = true);
+    osm_datasource(parameters const& params);
     virtual ~osm_datasource();
     mapnik::datasource::datasource_t type() const;
-    featureset_ptr features(const query& q) const;
+    featureset_ptr features(query const& q) const;
     featureset_ptr features_at_point(coord2d const& pt) const;
     box2d<double> envelope() const;
     boost::optional<mapnik::datasource::geometry_t> get_geometry_type() const;
     layer_descriptor get_descriptor() const;
     static std::string name();
-    void bind() const;
+
 private:
-    mutable box2d<double> extent_;
-    mutable osm_dataset* osm_data_;
+    void init(mapnik::parameters const& params);
+    box2d<double> extent_;
+    osm_dataset* osm_data_;
     mapnik::datasource::datasource_t type_;
-    mutable layer_descriptor desc_;
-    // no copying
-    osm_datasource(const osm_datasource&);
-    osm_datasource& operator=(const osm_datasource&);
+    layer_descriptor desc_;
 };
 
 #endif // OSM_DATASOURCE_HPP
