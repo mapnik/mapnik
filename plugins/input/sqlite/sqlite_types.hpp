34ddc65e308596f45fa86e52d9bb552eb2bb3b3a
Dane Springmeyer <dane@dbsgeo.com>
2012-03-14 15:08:39 -0700

    backport to 2.0.x series the sqlite3_open_v2 usage from master - refs #854

--- plugins/input/sqlite/sqlite_types.hpp
+++ plugins/input/sqlite/sqlite_types.hpp
@@ -142,27 +142,26 @@ public:
     sqlite_connection (const std::string& file)
         : db_(0)
     {
-        // sqlite3_open_v2 is available earlier but 
-        // shared cache not available until >= 3.6.18
-        #if SQLITE_VERSION_NUMBER >= 3006018
-        int rc = sqlite3_enable_shared_cache(1);
+#if SQLITE_VERSION_NUMBER >= 3005000
+        int mode = SQLITE_OPEN_READWRITE;
+#if SQLITE_VERSION_NUMBER >= 3006018
+        // shared cache flag not available until >= 3.6.18
+        mode |= SQLITE_OPEN_NOMUTEX | SQLITE_OPEN_SHAREDCACHE;
+#endif
+        const int rc = sqlite3_open_v2 (file_.c_str(), &db_, mode, 0);
+#else
+#warning "Mapnik's sqlite plugin is compiling against a version of sqlite older than 3.5.x which may make rendering slow..."
+        const int rc = sqlite3_open (file_.c_str(), &db_);
+#endif
         if (rc != SQLITE_OK)
         {
-           throw mapnik::datasource_exception (sqlite3_errmsg (db_));
-        }
-        
-        int mode = SQLITE_OPEN_READWRITE | SQLITE_OPEN_NOMUTEX | SQLITE_OPEN_SHAREDCACHE;
-        if (sqlite3_open_v2 (file.c_str(), &db_, mode, NULL))
-        #else
-        #warning "Mapnik's sqlite plugin is compiling against a version of sqlite older than 3.6.18 which may make rendering slow..."
-        if (sqlite3_open (file.c_str(), &db_))
-        #endif
-        {
             std::ostringstream s;
-            s << "Sqlite Plugin: ";
-            throw mapnik::datasource_exception (sqlite3_errmsg (db_));
+            s << "Sqlite Plugin: " << sqlite3_errmsg (db_);
+
+            throw mapnik::datasource_exception (s.str());
         }
-        //sqlite3_enable_load_extension(db_, 1);
+
+        sqlite3_busy_timeout(db_,5000);
     }
 
     ~sqlite_connection ()
