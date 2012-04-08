09f3728db0cc5676e3294e496d3b1ca9a8a64f60
kunitoki <kunitoki@gmail.com>
2012-04-08 03:56:32 +0200

    - fix a warning

--- src/markers_symbolizer.cpp
+++ src/markers_symbolizer.cpp
@@ -43,24 +43,24 @@ static const char * marker_type_strings[] = {
 IMPLEMENT_ENUM( marker_type_e, marker_type_strings )
 
 markers_symbolizer::markers_symbolizer()
-: symbolizer_with_image(path_expression_ptr(new path_expression)),
-    symbolizer_base(),
-    allow_overlap_(false),
-    ignore_placement_(false),
-    fill_(color(0,0,255)),
-    spacing_(100.0),
-    max_error_(0.2),
-    width_(10.0),
-    height_(10.0),
-    stroke_(),
-    marker_p_(MARKER_LINE_PLACEMENT),
-    marker_type_(ARROW) {}
+    : symbolizer_with_image(path_expression_ptr(new path_expression)),
+      symbolizer_base(),
+      ignore_placement_(false),
+      allow_overlap_(false),
+      fill_(color(0,0,255)),
+      spacing_(100.0),
+      max_error_(0.2),
+      width_(10.0),
+      height_(10.0),
+      stroke_(),
+      marker_p_(MARKER_LINE_PLACEMENT),
+      marker_type_(ARROW) {}
 
 markers_symbolizer::markers_symbolizer(path_expression_ptr filename)
     : symbolizer_with_image(filename),
       symbolizer_base(),
-      allow_overlap_(false),
       ignore_placement_(false),
+      allow_overlap_(false),
       fill_(color(0,0,255)),
       spacing_(100.0),
       max_error_(0.2),
@@ -73,8 +73,8 @@ markers_symbolizer::markers_symbolizer(path_expression_ptr filename)
 markers_symbolizer::markers_symbolizer(markers_symbolizer const& rhs)
     : symbolizer_with_image(rhs),
       symbolizer_base(rhs),
-      allow_overlap_(rhs.allow_overlap_),
       ignore_placement_(rhs.ignore_placement_),
+      allow_overlap_(rhs.allow_overlap_),
       fill_(rhs.fill_),
       spacing_(rhs.spacing_),
       max_error_(rhs.max_error_),
