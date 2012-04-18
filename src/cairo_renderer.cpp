2f65a7612d7f58f353f9f450f1707dc2b6130ba8
Dane Springmeyer <dane@dbsgeo.com>
2012-04-18 07:09:12 -0700

    allow cairo compile

--- src/cairo_renderer.cpp
+++ src/cairo_renderer.cpp
@@ -721,6 +721,20 @@ void cairo_renderer_base::start_map_processing(Map const& map)
 #endif
     }
 
+    void cairo_renderer_base::start_style_processing(feature_type_style const& st)
+    {
+    #ifdef MAPNIK_DEBUG
+        std::clog << "start style processing\n";
+    #endif
+    }
+    
+    void cairo_renderer_base::end_style_processing(feature_type_style const& st)
+    {
+    #ifdef MAPNIK_DEBUG
+        std::clog << "end style processing\n";
+    #endif
+    }
+
     void cairo_renderer_base::process(polygon_symbolizer const& sym,
                                       mapnik::feature_ptr const& feature,
                                       proj_transform const& prj_trans)
