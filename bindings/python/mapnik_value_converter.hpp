92dd30556a1af27bb8f9e5efdb60a14c56f2fec4
Dane Springmeyer <dane@dbsgeo.com>
2012-05-18 13:22:51 -0700

    python: fix leaky Py_None return by calling  Py_INCREF(Py_None) before returning - closes #1221

--- bindings/python/mapnik_value_converter.hpp
+++ bindings/python/mapnik_value_converter.hpp
@@ -59,6 +59,7 @@ namespace boost { namespace python {
             
         PyObject * operator() (mapnik::value_null const& /*s*/) const
         {
+            Py_INCREF(Py_None);
             return Py_None;
         }
     };
