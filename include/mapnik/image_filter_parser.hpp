6471cfc5978df78a423581d27669352426925ce9
Artem Pavlenko <artem@mapnik.org>
2012-05-23 20:23:10 +0100

    + unnamed args in image-filters #1222

--- include/mapnik/image_filter_parser.hpp
+++ include/mapnik/image_filter_parser.hpp
@@ -34,7 +34,7 @@ namespace qi = boost::spirit::qi;
 namespace phoenix = boost::phoenix;
 
 template <typename Iterator, typename ContType>
-struct image_filter_grammar : 
+struct image_filter_grammar :
         qi::grammar<Iterator, ContType(), qi::ascii::space_type>
 {
     image_filter_grammar()
@@ -59,7 +59,7 @@ struct image_filter_grammar :
             ;
 #endif
 
-        filter = 
+        filter =
             lit("emboss")[push_back(_val,construct<mapnik::filter::emboss>())]
             |
             lit("blur")[push_back(_val,construct<mapnik::filter::blur>())]
@@ -76,16 +76,16 @@ struct image_filter_grammar :
             |
             lit("y-gradient")[push_back(_val,construct<mapnik::filter::y_gradient>())]
             |
-            (lit("agg-stack-blur")[_a = 1, _b = 1] 
-             >> -( lit(':') >> lit("rx") >> lit('=') >> radius_[_a = _1] 
-                   >> lit(',') 
-                   >> lit("ry") >> lit('=') >> radius_[_b = _1])             
+            (lit("agg-stack-blur")[_a = 1, _b = 1]
+             >> -( lit(':') >> radius_[_a = _1]
+                   >> lit(',')
+                   >> radius_[_b = _1])
              [push_back(_val,construct<mapnik::filter::agg_stack_blur>(_a,_b))])
             |
             lit("invert")[push_back(_val,construct<mapnik::filter::invert>())]
             ;
     }
-    //  
+    //
     qi::rule<Iterator, ContType(), qi::ascii::space_type> start;
     qi::rule<Iterator, ContType(), qi::locals<int,int>, qi::ascii::space_type> filter;
     qi::uint_parser< unsigned, 10, 1, 3 > radius_;
