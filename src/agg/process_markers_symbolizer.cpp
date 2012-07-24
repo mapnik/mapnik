5ac4d2ec93546029cec9779e0423e597fdc08f1b
artemp <artem@mapnik.org>
2012-07-24 14:04:25 +0100

    + add raster markers support

--- src/agg/process_markers_symbolizer.cpp
+++ src/agg/process_markers_symbolizer.cpp
@@ -45,14 +45,18 @@
 #include "agg_path_storage.h"
 #include "agg_conv_clip_polyline.h"
 #include "agg_conv_transform.h"
-
+#include "agg_image_filters.h"
+#include "agg_trans_bilinear.h"
+#include "agg_span_allocator.h"
+#include "agg_image_accessors.h"
+#include "agg_span_image_filter_rgba.h"
 // boost
 #include <boost/optional.hpp>
 
 namespace mapnik {
 
 template <typename BufferType, typename SvgRenderer, typename Rasterizer, typename Detector>
-struct markers_rasterizer_dispatch
+struct vector_markers_rasterizer_dispatch
 {
     typedef agg::rgba8 color_type;
     typedef agg::order_rgba order_type;
@@ -62,7 +66,7 @@ struct markers_rasterizer_dispatch
     typedef agg::renderer_base<pixfmt_comp_type> renderer_base;
     typedef agg::renderer_scanline_aa_solid<renderer_base> renderer_type;
 
-    markers_rasterizer_dispatch(BufferType & image_buffer,
+    vector_markers_rasterizer_dispatch(BufferType & image_buffer,
                                 SvgRenderer & svg_renderer,
                                 Rasterizer & ras,
                                 box2d<double> const& bbox,
@@ -91,7 +95,6 @@ struct markers_rasterizer_dispatch
 
         if (placement_method == MARKER_POINT_PLACEMENT)
         {
-
             double x,y;
             path.rewind(0);
             label::interior_position(path, x, y);
@@ -124,7 +127,6 @@ struct markers_rasterizer_dispatch
             }
         }
     }
-
 private:
     agg::scanline_u8 sl_;
     agg::rendering_buffer buf_;
@@ -139,11 +141,61 @@ private:
     double scale_factor_;
 };
 
+template <typename Rasterizer, typename RendererBuffer>
+void render_raster_marker(Rasterizer & ras, RendererBuffer & renb, agg::scanline_u8 & sl,
+                          pixel_position const& pos, image_data_32 const& src,
+                          agg::trans_affine const& tr,double opacity,
+                          double scale_factor)
+{
 
-template <typename T>
-void agg_renderer<T>::process(markers_symbolizer const& sym,
-                              mapnik::feature_impl & feature,
-                              proj_transform const& prj_trans)
+    double width  = src.width();
+    double height = src.height();
+    double p[8];
+    p[0] = pos.x;         p[1] = pos.y;
+    p[2] = pos.x + width; p[3] = pos.y;
+    p[4] = pos.x + width; p[5] = pos.y + height;
+    p[6] = pos.x;         p[7] = pos.y + height;
+
+    agg::trans_affine marker_tr;
+
+    marker_tr *= agg::trans_affine_translation(-pos.x,-pos.y);
+    marker_tr *= tr;
+    marker_tr *= agg::trans_affine_scaling(scale_factor);
+    marker_tr *= agg::trans_affine_translation(pos.x,pos.y);
+
+    marker_tr.transform(&p[0], &p[1]);
+    marker_tr.transform(&p[2], &p[3]);
+    marker_tr.transform(&p[4], &p[5]);
+    marker_tr.transform(&p[6], &p[7]);
+
+    ras.move_to_d(p[0],p[1]);
+    ras.line_to_d(p[2],p[3]);
+    ras.line_to_d(p[4],p[5]);
+    ras.line_to_d(p[6],p[7]);
+
+    typedef agg::rgba8 color_type;
+    agg::span_allocator<color_type> sa;
+    agg::image_filter_bilinear filter_kernel;
+    agg::image_filter_lut filter(filter_kernel, false);
+
+    agg::rendering_buffer marker_buf((unsigned char *)src.getBytes(),
+                                     src.width(),
+                                     src.height(),
+                                     src.width()*4);
+    agg::pixfmt_rgba32_pre pixf(marker_buf);
+
+    typedef agg::image_accessor_clone<agg::pixfmt_rgba32_pre> img_accessor_type;
+    typedef agg::span_interpolator_linear<agg::trans_affine> interpolator_type;
+    typedef agg::span_image_filter_rgba_2x2<img_accessor_type,
+                                            interpolator_type> span_gen_type;
+    img_accessor_type ia(pixf);
+    interpolator_type interpolator(agg::trans_affine(p, 0, 0, width, height) );
+    span_gen_type sg(ia, interpolator, filter);
+    agg::render_scanlines_aa(ras, sl, renb, sa, sg);
+}
+
+template <typename BufferType, typename Rasterizer, typename Detector>
+struct raster_markers_rasterizer_dispatch
 {
     typedef agg::rgba8 color_type;
     typedef agg::order_rgba order_type;
@@ -153,6 +205,96 @@ void agg_renderer<T>::process(markers_symbolizer const& sym,
     typedef agg::renderer_base<pixfmt_comp_type> renderer_base;
     typedef agg::renderer_scanline_aa_solid<renderer_base> renderer_type;
 
+    raster_markers_rasterizer_dispatch(BufferType & image_buffer,
+                                       Rasterizer & ras,
+                                       image_data_32 const& src,
+                                       agg::trans_affine const& marker_trans,
+                                       markers_symbolizer const& sym,
+                                       Detector & detector,
+                                       double scale_factor)
+        : buf_(image_buffer.raw_data(), image_buffer.width(), image_buffer.height(), image_buffer.width() * 4),
+        pixf_(buf_),
+        renb_(pixf_),
+        ras_(ras),
+        src_(src),
+        marker_trans_(marker_trans),
+        sym_(sym),
+        detector_(detector),
+        scale_factor_(scale_factor)
+    {
+        pixf_.comp_op(static_cast<agg::comp_op_e>(sym_.comp_op()));
+    }
+
+    template <typename T>
+    void add_path(T & path)
+    {
+        marker_placement_e placement_method = sym_.get_marker_placement();
+        box2d<double> bbox_(0,0, src_.width(),src_.height());
+
+        if (placement_method == MARKER_POINT_PLACEMENT)
+        {
+            double x,y;
+            path.rewind(0);
+            label::interior_position(path, x, y);
+            agg::trans_affine matrix = marker_trans_;
+            matrix.translate(x,y);
+            box2d<double> transformed_bbox = bbox_ * matrix;
+
+            if (sym_.get_allow_overlap() ||
+                detector_.has_placement(transformed_bbox))
+            {
+
+                render_raster_marker(ras_, renb_, sl_, pixel_position(x,y), src_,
+                                     marker_trans_, sym_.get_opacity(), scale_factor_);
+                if (!sym_.get_ignore_placement())
+                    detector_.insert(transformed_bbox);
+            }
+        }
+        else
+        {
+            markers_placement<T, label_collision_detector4> placement(path, bbox_, marker_trans_, detector_,
+                                                                      sym_.get_spacing() * scale_factor_,
+                                                                      sym_.get_max_error(),
+                                                                      sym_.get_allow_overlap());
+            double x, y, angle;
+            while (placement.get_point(x, y, angle))
+            {
+                agg::trans_affine matrix = marker_trans_;
+                matrix.rotate(angle);
+                render_raster_marker(ras_, renb_, sl_,  pixel_position(x,y), src_,
+                                     matrix, sym_.get_opacity(), scale_factor_);
+            }
+        }
+    }
+private:
+    agg::scanline_u8 sl_;
+    agg::rendering_buffer buf_;
+    pixfmt_comp_type pixf_;
+    renderer_base renb_;
+    Rasterizer & ras_;
+    image_data_32 const& src_;
+    agg::trans_affine const& marker_trans_;
+    markers_symbolizer const& sym_;
+    Detector & detector_;
+    double scale_factor_;
+};
+
+
+template <typename T>
+void agg_renderer<T>::process(markers_symbolizer const& sym,
+                              feature_impl & feature,
+                              proj_transform const& prj_trans)
+{
+    typedef agg::rgba8 color_type;
+    typedef agg::order_rgba order_type;
+    typedef agg::pixel32_type pixel_type;
+    typedef agg::comp_op_adaptor_rgba_pre<color_type, order_type> blender_type; // comp blender
+    typedef agg::pixfmt_custom_blend_rgba<blender_type, agg::rendering_buffer> pixfmt_comp_type;
+    typedef agg::renderer_base<pixfmt_comp_type> renderer_base;
+    typedef agg::renderer_scanline_aa_solid<renderer_base> renderer_type;
+    typedef label_collision_detector4 detector_type;
+    typedef boost::mpl::vector<clip_line_tag,transform_tag,smooth_tag> conv_types;
+
     std::string filename = path_processor_type::evaluate(*sym.get_filename(), feature);
 
     if (!filename.empty())
@@ -160,61 +302,76 @@ void agg_renderer<T>::process(markers_symbolizer const& sym,
         boost::optional<marker_ptr> mark = mapnik::marker_cache::instance()->find(filename, true);
         if (mark && *mark)
         {
-            if (!(*mark)->is_vector())
-            {
-                MAPNIK_LOG_DEBUG(agg_renderer) << "agg_renderer: markers_symbolizer does not yet support non-SVG markers";
-                return;
-            }
-
             ras_ptr->reset();
             ras_ptr->gamma(agg::gamma_power());
 
             agg::trans_affine geom_tr;
             evaluate_transform(geom_tr, feature, sym.get_transform());
 
-            boost::optional<path_ptr> marker = (*mark)->get_vector_data();
-            box2d<double> const& bbox = (*marker)->bounding_box();
-
+            box2d<double> const& bbox = (*mark)->bounding_box();
             agg::trans_affine tr;
             setup_label_transform(tr, bbox, feature, sym);
             tr = agg::trans_affine_scaling(scale_factor_) * tr;
-
             coord2d center = bbox.center();
             agg::trans_affine_translation recenter(-center.x, -center.y);
             agg::trans_affine marker_trans = recenter * tr;
 
-            using namespace mapnik::svg;
-            vertex_stl_adapter<svg_path_storage> stl_storage((*marker)->source());
-            svg_path_adapter svg_path(stl_storage);
-
-            agg::pod_bvector<path_attributes> attributes;
-            bool result = push_explicit_style( (*marker)->attributes(), attributes, sym);
-
-            typedef label_collision_detector4 detector_type;
-            typedef svg_renderer<svg_path_adapter,
-                                 agg::pod_bvector<path_attributes>,
-                                 renderer_type,
-                                 agg::pixfmt_rgba32 > svg_renderer_type;
-            typedef markers_rasterizer_dispatch<buffer_type, svg_renderer_type, rasterizer, detector_type>  markers_rasterizer_dispatch_type;
-            typedef boost::mpl::vector<clip_line_tag,transform_tag,smooth_tag> conv_types;
-
-            svg_renderer_type svg_renderer(svg_path, result ? attributes : (*marker)->attributes());
-
-            markers_rasterizer_dispatch_type rasterizer_dispatch(*current_buffer_,svg_renderer,*ras_ptr,
-                                                                 bbox, marker_trans, sym, *detector_, scale_factor_);
-
-
-            vertex_converter<box2d<double>, markers_rasterizer_dispatch_type, markers_symbolizer,
-                             CoordTransform, proj_transform, agg::trans_affine, conv_types>
-                converter(query_extent_* 1.1,rasterizer_dispatch, sym,t_,prj_trans,tr,scale_factor_);
-
-            if (sym.clip()) converter.template set<clip_line_tag>(); //optional clip (default: true)
-            converter.template set<transform_tag>(); //always transform
-            if (sym.smooth() > 0.0) converter.template set<smooth_tag>(); // optional smooth converter
-
-            BOOST_FOREACH(geometry_type & geom, feature.paths())
+            if ((*mark)->is_vector())
             {
-                converter.apply(geom);
+                using namespace mapnik::svg;
+                boost::optional<path_ptr> marker = (*mark)->get_vector_data();
+
+
+                vertex_stl_adapter<svg_path_storage> stl_storage((*marker)->source());
+                svg_path_adapter svg_path(stl_storage);
+
+                agg::pod_bvector<path_attributes> attributes;
+                bool result = push_explicit_style( (*marker)->attributes(), attributes, sym);
+
+                typedef svg_renderer<svg_path_adapter,
+                                     agg::pod_bvector<path_attributes>,
+                                     renderer_type,
+                                     agg::pixfmt_rgba32 > svg_renderer_type;
+                typedef vector_markers_rasterizer_dispatch<buffer_type, svg_renderer_type, rasterizer, detector_type> dispatch_type;
+
+
+                svg_renderer_type svg_renderer(svg_path, result ? attributes : (*marker)->attributes());
+
+                dispatch_type rasterizer_dispatch(*current_buffer_,svg_renderer,*ras_ptr,
+                                                  bbox, marker_trans, sym, *detector_, scale_factor_);
+
+
+                vertex_converter<box2d<double>, dispatch_type, markers_symbolizer,
+                                 CoordTransform, proj_transform, agg::trans_affine, conv_types>
+                    converter(query_extent_* 1.1,rasterizer_dispatch, sym,t_,prj_trans,tr,scale_factor_);
+
+                if (sym.clip()) converter.template set<clip_line_tag>(); //optional clip (default: true)
+                converter.template set<transform_tag>(); //always transform
+                if (sym.smooth() > 0.0) converter.template set<smooth_tag>(); // optional smooth converter
+
+                BOOST_FOREACH(geometry_type & geom, feature.paths())
+                {
+                    converter.apply(geom);
+                }
+            }
+            else // raster markers
+            {
+                boost::optional<mapnik::image_ptr> marker = (*mark)->get_bitmap_data();
+                typedef raster_markers_rasterizer_dispatch<buffer_type,rasterizer, detector_type> dispatch_type;
+                dispatch_type rasterizer_dispatch(*current_buffer_,*ras_ptr, **marker,
+                                                  marker_trans, sym, *detector_, scale_factor_);
+                vertex_converter<box2d<double>, dispatch_type, markers_symbolizer,
+                                 CoordTransform, proj_transform, agg::trans_affine, conv_types>
+                    converter(query_extent_* 1.1, rasterizer_dispatch, sym,t_,prj_trans,tr,scale_factor_);
+
+                if (sym.clip()) converter.template set<clip_line_tag>(); //optional clip (default: true)
+                converter.template set<transform_tag>(); //always transform
+                if (sym.smooth() > 0.0) converter.template set<smooth_tag>(); // optional smooth converter
+
+                BOOST_FOREACH(geometry_type & geom, feature.paths())
+                {
+                    converter.apply(geom);
+                }
             }
         }
     }
