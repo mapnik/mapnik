mapnik/mapnik

commit 822531a114a82090284139716706270c7b4a2949

+ filter on geometry type to use centroid (Polygon) or   middle_point (LineString) label placement

src/symbolizer_helpers.cpp
@@ -244,7 +244,14 @@ void text_symbolizer_helper<FaceManagerT, DetectorT>::initialize_points()
         {
             if (how_placed == POINT_PLACEMENT)
             {
-                label::centroid(geom, label_x, label_y);
+                if (geom.type() == Polygon)
+                {
+                    label::centroid(geom, label_x, label_y);
+                }
+                else if (geom.type() == LineString)
+                {
+                    label::middle_point(geom, label_x,label_y);
+                }
             }
             else if (how_placed == INTERIOR_PLACEMENT)
             {

commit 6f174287a2d2b32e8fd799d7f4a6979e7f2c71b8

amend 822531a which broke polygon geometry labeling - refs #1350

src/symbolizer_helpers.cpp
@@ -242,16 +242,13 @@ void text_symbolizer_helper<FaceManagerT, DetectorT>::initialize_points()
         }
         else
         {
-            if (how_placed == POINT_PLACEMENT)
+            if (geom.type() == LineString)
             {
-                if (geom.type() == Polygon)
-                {
-                    label::centroid(geom, label_x, label_y);
-                }
-                else if (geom.type() == LineString)
-                {
-                    label::middle_point(geom, label_x,label_y);
-                }
+                label::middle_point(geom, label_x,label_y);
+            }
+            else if (how_placed == POINT_PLACEMENT)
+            {
+                label::centroid(geom, label_x, label_y);
             }
             else if (how_placed == INTERIOR_PLACEMENT)
             {