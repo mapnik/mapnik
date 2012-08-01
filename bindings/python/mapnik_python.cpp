a9f99848c238a4c0a4ef461b91abdca77d25fe87
Dane Springmeyer <dane@dbsgeo.com>
2012-07-31 18:40:53 -0700

    expose boost::optional<mapnik::stroke> for markers_symbolizer (which works as oopposed to boost::optional<double> and boost::optional<float> which are broken

--- bindings/python/mapnik_python.cpp
+++ bindings/python/mapnik_python.cpp
@@ -608,6 +608,7 @@ BOOST_PYTHON_MODULE(_mapnik)
     def("has_cairo", &has_cairo, "Get cairo library status");
     def("has_pycairo", &has_pycairo, "Get pycairo module status");
 
+    python_optional<mapnik::stroke>();
     python_optional<mapnik::color>();
     python_optional<mapnik::box2d<double> >();
     python_optional<mapnik::datasource::geometry_t>();
