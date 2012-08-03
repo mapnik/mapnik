cd73d391f7a30f96e2cf9614fc22943c615d8661
Dane Springmeyer <dane@dbsgeo.com>
2012-08-03 09:43:33 -0700

    unmask second, previously crashing svg rendering/compositing test

--- tests/python_tests/markers_complex_rendering_test.py
+++ tests/python_tests/markers_complex_rendering_test.py
@@ -21,18 +21,18 @@ def test_marker_ellipse_render1():
     expected_im = mapnik.Image.open(expected)
     eq_(im.tostring(),expected_im.tostring(), 'failed comparing actual (%s) and expected (%s)' % (actual,'tests/python_tests/'+ expected))
 
-#def test_marker_ellipse_render2():
-#    # currently crashes https://github.com/mapnik/mapnik/issues/1365
-#    m = mapnik.Map(256,256)
-#    mapnik.load_map(m,'../data/good_maps/marker_ellipse_transform2.xml')
-#    m.zoom_all()
-#    im = mapnik.Image(m.width,m.height)
-#    mapnik.render(m,im)
-#    actual = '/tmp/mapnik-marker-ellipse-render2.png'
-#    expected = 'images/support/mapnik-marker-ellipse-render2.png'
-#    im.save(actual)
-#    expected_im = mapnik.Image.open(expected)
-#    eq_(im.tostring(),expected_im.tostring(), 'failed comparing actual (%s) and expected (%s)' % (actual,'tests/python_tests/'+ expected))
+def test_marker_ellipse_render2():
+    # currently crashes https://github.com/mapnik/mapnik/issues/1365
+    m = mapnik.Map(256,256)
+    mapnik.load_map(m,'../data/good_maps/marker_ellipse_transform2.xml')
+    m.zoom_all()
+    im = mapnik.Image(m.width,m.height)
+    mapnik.render(m,im)
+    actual = '/tmp/mapnik-marker-ellipse-render2.png'
+    expected = 'images/support/mapnik-marker-ellipse-render2.png'
+    im.save(actual)
+    expected_im = mapnik.Image.open(expected)
+    eq_(im.tostring(),expected_im.tostring(), 'failed comparing actual (%s) and expected (%s)' % (actual,'tests/python_tests/'+ expected))
 
 if __name__ == "__main__":
     setup()
