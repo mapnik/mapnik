8ea21d866d8c966c214ea9c602c84b6ba23d5806
Dane Springmeyer <dane@dbsgeo.com>
2012-08-02 22:52:21 -0700

    move more code to marker_helpers.hpp to set up for grid impl of new markers code - refs #1282

--- src/agg/process_markers_symbolizer.cpp
+++ src/agg/process_markers_symbolizer.cpp
@@ -29,18 +29,19 @@
 #include <mapnik/geom_util.hpp>
 #include <mapnik/expression_evaluator.hpp>
 #include <mapnik/vertex_converters.hpp>
+#include <mapnik/marker_helpers.hpp>
 #include <mapnik/marker.hpp>
 #include <mapnik/marker_cache.hpp>
-#include <mapnik/marker_helpers.hpp>
 #include <mapnik/svg/svg_renderer.hpp>
 #include <mapnik/svg/svg_storage.hpp>
 #include <mapnik/svg/svg_path_adapter.hpp>
 #include <mapnik/svg/svg_path_attributes.hpp>
-#include <mapnik/markers_placement.hpp>
 #include <mapnik/markers_symbolizer.hpp>
 
 // agg
 #include "agg_basics.h"
+#include "agg_renderer_base.h"
+#include "agg_renderer_scanline.h"
 #include "agg_rendering_buffer.h"
 #include "agg_pixfmt_rgba.h"
 #include "agg_rasterizer_scanline_aa.h"
@@ -48,256 +49,12 @@
 #include "agg_path_storage.h"
 #include "agg_conv_clip_polyline.h"
 #include "agg_conv_transform.h"
-#include "agg_image_filters.h"
-#include "agg_trans_bilinear.h"
-#include "agg_span_allocator.h"
-#include "agg_image_accessors.h"
-#include "agg_span_image_filter_rgba.h"
+
 
 // boost
 #include <boost/optional.hpp>
 
 namespace mapnik {
-
-template <typename BufferType, typename SvgRenderer, typename Rasterizer, typename Detector>
-struct vector_markers_rasterizer_dispatch
-{
-    typedef agg::rgba8 color_type;
-    typedef agg::order_rgba order_type;
-    typedef agg::pixel32_type pixel_type;
-    typedef agg::comp_op_adaptor_rgba_pre<color_type, order_type> blender_type; // comp blender
-    typedef agg::pixfmt_custom_blend_rgba<blender_type, agg::rendering_buffer> pixfmt_comp_type;
-    typedef agg::renderer_base<pixfmt_comp_type> renderer_base;
-
-    vector_markers_rasterizer_dispatch(BufferType & image_buffer,
-                                SvgRenderer & svg_renderer,
-                                Rasterizer & ras,
-                                box2d<double> const& bbox,
-                                agg::trans_affine const& marker_trans,
-                                markers_symbolizer const& sym,
-                                Detector & detector,
-                                double scale_factor)
-        : buf_(image_buffer.raw_data(), image_buffer.width(), image_buffer.height(), image_buffer.width() * 4),
-        pixf_(buf_),
-        renb_(pixf_),
-        svg_renderer_(svg_renderer),
-        ras_(ras),
-        bbox_(bbox),
-        marker_trans_(marker_trans),
-        sym_(sym),
-        detector_(detector),
-        scale_factor_(scale_factor)
-    {
-        pixf_.comp_op(static_cast<agg::comp_op_e>(sym_.comp_op()));
-    }
-
-    template <typename T>
-    void add_path(T & path)
-    {
-        marker_placement_e placement_method = sym_.get_marker_placement();
-
-        if (placement_method != MARKER_LINE_PLACEMENT)
-        {
-            double x,y;
-            path.rewind(0);
-            if (placement_method == MARKER_INTERIOR_PLACEMENT)
-            {
-                label::interior_position(path, x, y);
-            }
-            else
-            {
-                label::centroid(path, x, y);
-            }
-            agg::trans_affine matrix = marker_trans_;
-            matrix.translate(x,y);
-            box2d<double> transformed_bbox = bbox_ * matrix;
-
-            if (sym_.get_allow_overlap() ||
-                detector_.has_placement(transformed_bbox))
-            {
-                svg_renderer_.render(ras_, sl_, renb_, matrix, 1, bbox_);
-
-                if (!sym_.get_ignore_placement())
-                    detector_.insert(transformed_bbox);
-            }
-        }
-        else
-        {
-            markers_placement<T, label_collision_detector4> placement(path, bbox_, marker_trans_, detector_,
-                                                                      sym_.get_spacing() * scale_factor_,
-                                                                      sym_.get_max_error(),
-                                                                      sym_.get_allow_overlap());
-            double x, y, angle;
-            while (placement.get_point(x, y, angle))
-            {
-                agg::trans_affine matrix = marker_trans_;
-                matrix.rotate(angle);
-                matrix.translate(x, y);
-                svg_renderer_.render(ras_, sl_, renb_, matrix, 1, bbox_);
-            }
-        }
-    }
-private:
-    agg::scanline_u8 sl_;
-    agg::rendering_buffer buf_;
-    pixfmt_comp_type pixf_;
-    renderer_base renb_;
-    SvgRenderer & svg_renderer_;
-    Rasterizer & ras_;
-    box2d<double> const& bbox_;
-    agg::trans_affine const& marker_trans_;
-    markers_symbolizer const& sym_;
-    Detector & detector_;
-    double scale_factor_;
-};
-
-template <typename Rasterizer, typename RendererBuffer>
-void render_raster_marker(Rasterizer & ras, RendererBuffer & renb,
-                          agg::scanline_u8 & sl, image_data_32 const& src,
-                          agg::trans_affine const& marker_tr, double opacity)
-{
-    double width  = src.width();
-    double height = src.height();
-    double p[8];
-    p[0] = 0;     p[1] = 0;
-    p[2] = width; p[3] = 0;
-    p[4] = width; p[5] = height;
-    p[6] = 0;     p[7] = height;
-
-    marker_tr.transform(&p[0], &p[1]);
-    marker_tr.transform(&p[2], &p[3]);
-    marker_tr.transform(&p[4], &p[5]);
-    marker_tr.transform(&p[6], &p[7]);
-
-    ras.move_to_d(p[0],p[1]);
-    ras.line_to_d(p[2],p[3]);
-    ras.line_to_d(p[4],p[5]);
-    ras.line_to_d(p[6],p[7]);
-
-    typedef agg::rgba8 color_type;
-    agg::span_allocator<color_type> sa;
-    agg::image_filter_bilinear filter_kernel;
-    agg::image_filter_lut filter(filter_kernel, false);
-
-    agg::rendering_buffer marker_buf((unsigned char *)src.getBytes(),
-                                     src.width(),
-                                     src.height(),
-                                     src.width()*4);
-    agg::pixfmt_rgba32_pre pixf(marker_buf);
-
-    typedef agg::image_accessor_clone<agg::pixfmt_rgba32_pre> img_accessor_type;
-    typedef agg::span_interpolator_linear<agg::trans_affine> interpolator_type;
-    typedef agg::span_image_filter_rgba_2x2<img_accessor_type,
-                                            interpolator_type> span_gen_type;
-    typedef agg::order_rgba order_type;
-    typedef agg::pixel32_type pixel_type;
-    typedef agg::comp_op_adaptor_rgba_pre<color_type, order_type> blender_type; // comp blender
-    typedef agg::pixfmt_custom_blend_rgba<blender_type, agg::rendering_buffer> pixfmt_comp_type;
-    typedef agg::renderer_base<pixfmt_comp_type> renderer_base;
-    typedef agg::renderer_scanline_aa_alpha<renderer_base,
-            agg::span_allocator<agg::rgba8>,
-            span_gen_type> renderer_type;
-    img_accessor_type ia(pixf);
-    interpolator_type interpolator(agg::trans_affine(p, 0, 0, width, height) );
-    span_gen_type sg(ia, interpolator, filter);
-    renderer_type rp(renb,sa, sg, unsigned(opacity*255));
-    agg::render_scanlines(ras, sl, rp);
-}
-
-template <typename BufferType, typename Rasterizer, typename Detector>
-struct raster_markers_rasterizer_dispatch
-{
-    typedef agg::rgba8 color_type;
-    typedef agg::order_rgba order_type;
-    typedef agg::pixel32_type pixel_type;
-    typedef agg::comp_op_adaptor_rgba_pre<color_type, order_type> blender_type; // comp blender
-    typedef agg::pixfmt_custom_blend_rgba<blender_type, agg::rendering_buffer> pixfmt_comp_type;
-    typedef agg::renderer_base<pixfmt_comp_type> renderer_base;
-
-    raster_markers_rasterizer_dispatch(BufferType & image_buffer,
-                                       Rasterizer & ras,
-                                       image_data_32 const& src,
-                                       agg::trans_affine const& marker_trans,
-                                       markers_symbolizer const& sym,
-                                       Detector & detector,
-                                       double scale_factor)
-        : buf_(image_buffer.raw_data(), image_buffer.width(), image_buffer.height(), image_buffer.width() * 4),
-        pixf_(buf_),
-        renb_(pixf_),
-        ras_(ras),
-        src_(src),
-        marker_trans_(marker_trans),
-        sym_(sym),
-        detector_(detector),
-        scale_factor_(scale_factor)
-    {
-        pixf_.comp_op(static_cast<agg::comp_op_e>(sym_.comp_op()));
-    }
-
-    template <typename T>
-    void add_path(T & path)
-    {
-        marker_placement_e placement_method = sym_.get_marker_placement();
-        box2d<double> bbox_(0,0, src_.width(),src_.height());
-
-        if (placement_method != MARKER_LINE_PLACEMENT)
-        {
-            double x,y;
-            path.rewind(0);
-            if (placement_method == MARKER_INTERIOR_PLACEMENT)
-            {
-                label::interior_position(path, x, y);
-            }
-            else
-            {
-                label::centroid(path, x, y);
-            }
-            agg::trans_affine matrix = marker_trans_;
-            matrix.translate(x,y);
-            box2d<double> transformed_bbox = bbox_ * matrix;
-
-            if (sym_.get_allow_overlap() ||
-                detector_.has_placement(transformed_bbox))
-            {
-
-                float opacity = sym_.get_opacity() ? *sym_.get_opacity() : 1;
-                render_raster_marker(ras_, renb_, sl_, src_,
-                                     matrix, opacity);
-                if (!sym_.get_ignore_placement())
-                    detector_.insert(transformed_bbox);
-            }
-        }
-        else
-        {
-            markers_placement<T, label_collision_detector4> placement(path, bbox_, marker_trans_, detector_,
-                                                                      sym_.get_spacing() * scale_factor_,
-                                                                      sym_.get_max_error(),
-                                                                      sym_.get_allow_overlap());
-            double x, y, angle;
-            while (placement.get_point(x, y, angle))
-            {
-                agg::trans_affine matrix = marker_trans_;
-                matrix.rotate(angle);
-                matrix.translate(x,y);
-                float opacity = sym_.get_opacity() ? *sym_.get_opacity() : 1;
-                render_raster_marker(ras_, renb_, sl_, src_,
-                                     matrix, opacity);
-            }
-        }
-    }
-private:
-    agg::scanline_u8 sl_;
-    agg::rendering_buffer buf_;
-    pixfmt_comp_type pixf_;
-    renderer_base renb_;
-    Rasterizer & ras_;
-    image_data_32 const& src_;
-    agg::trans_affine const& marker_trans_;
-    markers_symbolizer const& sym_;
-    Detector & detector_;
-    double scale_factor_;
-};
-
 
 template <typename T>
 void agg_renderer<T>::process(markers_symbolizer const& sym,
