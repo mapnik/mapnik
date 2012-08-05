e437ae7aeb71c03ba4890285e9670b3f0d2c01c4
Hermann Kraus <hermr2d2@gmail.com>
2012-08-05 15:34:22 +0200

    Fix crash while processing empty ways in OSM data (e.g. deleted ways).

--- plugins/input/osm/osm_featureset.cpp
+++ plugins/input/osm/osm_featureset.cpp
@@ -27,6 +27,7 @@
 // mapnik
 #include <mapnik/geometry.hpp>
 #include <mapnik/feature_factory.hpp>
+#include <mapnik/debug.hpp>
 
 #include "osm_featureset.hpp"
 
@@ -61,7 +62,7 @@ feature_ptr osm_featureset<filterT>::next()
     if (!cur_item) return feature_ptr();
     if (dataset_->current_item_is_node())
     {
-        feature = feature_factory::create(ctx_,feature_id_);
+        feature = feature_factory::create(ctx_, feature_id_);
         ++feature_id_;
         double lat = static_cast<osm_node*>(cur_item)->lat;
         double lon = static_cast<osm_node*>(cur_item)->lon;
@@ -75,14 +76,14 @@ feature_ptr osm_featureset<filterT>::next()
         while (cur_item)
         {
             bounds b = static_cast<osm_way*>(cur_item)->get_bounds();
-            if (filter_.pass(box2d<double>(b.w, b.s, b.e, b.n))) break;
+            if (filter_.pass(box2d<double>(b.w, b.s, b.e, b.n))
+                    &&
+                static_cast<osm_way*>(cur_item)->nodes.size()) break;
             cur_item = dataset_->next_item();
         }
 
         if (!cur_item) return feature_ptr();
-        if (static_cast<osm_way*>(cur_item)->nodes.size())
-        {
-            feature = feature_factory::create(ctx_,feature_id_);
+            feature = feature_factory::create(ctx_, feature_id_);
             ++feature_id_;
             geometry_type* geom;
             if (static_cast<osm_way*>(cur_item)->is_polygon())
@@ -105,19 +106,22 @@ feature_ptr osm_featureset<filterT>::next()
                               static_cast<osm_way*>(cur_item)->nodes[count]->lat);
             }
             feature->add_geometry(geom);
-        }
+    } else
+    {
+        MAPNIK_LOG_FATAL(osm_featureset) << "Current item is neither node nor way.\n";
     }
 
     std::set<std::string>::const_iterator itr = attribute_names_.begin();
     std::set<std::string>::const_iterator end = attribute_names_.end();
     std::map<std::string,std::string>::iterator end_keyvals = cur_item->keyvals.end();
-
     for (; itr != end; itr++)
     {
         std::map<std::string,std::string>::iterator i = cur_item->keyvals.find(*itr);
-        if (i != end_keyvals) {
-            feature->put_new(i->first,tr_->transcode(i->second.c_str()));
-        } else {
+        if (i != end_keyvals)
+        {
+            feature->put_new(i->first, tr_->transcode(i->second.c_str()));
+        } else
+        {
             feature->put_new(*itr, "");
         }
     }
