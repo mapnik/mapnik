12f26614e52180a7d2997691e71cb816f192efc5
Dane Springmeyer <dane@dbsgeo.com>
2012-09-11 19:29:45 -0700

    add failing testcase for #1471 (expected image generated with patch from #1471 that disables premultiplication of data raster before blending)

--- tests/python_tests/raster_alpha_test.py
+++ tests/python_tests/raster_alpha_test.py
@@ -36,6 +36,20 @@ if 'gdal' in mapnik.DatasourceCache.plugin_names():
         expected_im = mapnik.Image.open(expected)
         eq_(im.tostring(),expected_im.tostring(), 'failed comparing actual (%s) and expected(%s)' % (actual,'tests/python_tests/'+ expected))
 
+    # there should be no gray edges on raster
+    # https://github.com/mapnik/mapnik/issues/1471
+    def test_edge_scaling_with_nodata():
+        m = mapnik.Map(600,400)
+        mapnik.load_map(m,'../data/good_maps/raster-nodata-edge.xml')
+        m.zoom_all()
+        actual = '/tmp/mapnik-raster-nodata-edge.png'
+        expected = 'images/support/raster-nodata-edge.png'
+        im = mapnik.Image(m.width,m.height)
+        mapnik.render(m,im)
+        im.save(actual)
+        expected_im = mapnik.Image.open(expected)
+        eq_(im.tostring(),expected_im.tostring(), 'failed comparing actual (%s) and expected(%s)' % (actual,'tests/python_tests/'+ expected))
+
 
 if __name__ == "__main__":
     setup()
