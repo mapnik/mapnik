e221168e4ef8b97a4a4cc5c09c66e35dffc876f1
Dane Springmeyer <dane@dbsgeo.com>
2012-09-26 12:56:57 -0700

    after #1483 fontset is boost optional so check appropriately in load_map (solves assert)

--- src/load_map.cpp
+++ src/load_map.cpp
@@ -1148,7 +1148,7 @@ void map_parser::parse_text_symbolizer(rule & rule, xml_node const& sym)
             placement_finder->defaults.from_xml(sym, fontsets_);
         }
         if (strict_ &&
-            !placement_finder->defaults.format.fontset->size())
+            !placement_finder->defaults.format.fontset)
         {
             ensure_font_face(placement_finder->defaults.format.face_name);
         }
@@ -1177,7 +1177,7 @@ void map_parser::parse_shield_symbolizer(rule & rule, xml_node const& sym)
         }
         placement_finder->defaults.from_xml(sym, fontsets_);
         if (strict_ &&
-            !placement_finder->defaults.format.fontset->size())
+            !placement_finder->defaults.format.fontset)
         {
             ensure_font_face(placement_finder->defaults.format.face_name);
         }
