8bb0058717abfb97a99cf48d6995096c854ccbcf
Dane Springmeyer <dane@dbsgeo.com>
2012-11-08 18:52:55 -0500

    uncomment png test

--- tests/python_tests/png_encoding_test.py
+++ tests/python_tests/png_encoding_test.py
@@ -130,7 +130,7 @@ def test_transparency_levels_aerial():
 
     im_in = mapnik.Image.open('./images/support/transparency/aerial_rgb.png')
     eq_(len(im.tostring('png')),len(im_in.tostring('png')))
-    #eq_(len(im.tostring('png:t=0')),len(im_in.tostring('png:t=0')))
+    eq_(len(im.tostring('png:t=0')),len(im_in.tostring('png:t=0')))
 
 
 if __name__ == "__main__":
