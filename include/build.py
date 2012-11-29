1f7459d29d82578e2454fa7bfc39293c1116137d
Dane Springmeyer <dane@dbsgeo.com>
2012-11-28 17:21:38 -0800

    install svg/output headers if SVG_RENDERER is requested

--- include/build.py
+++ include/build.py
@@ -6,8 +6,8 @@ Import('env')
 base = './mapnik/'
 subdirs = ['','svg','wkt','grid','json','util','text_placements','formatting']
 
-#if env['SVG_RENDERER']:
-#    subdirs.append('svg/output')
+if env['SVG_RENDERER']:
+    subdirs.append('svg/output')
 
 if 'install' in COMMAND_LINE_TARGETS:
     for subdir in subdirs:
