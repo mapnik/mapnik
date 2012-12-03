46d602635683a1cb6a18ddadcdafeb5e0d4f6bd5
artemp <artem@mapnik.org>
2012-12-03 17:47:59 +0000

    + verify content lengths

--- utils/shapefile/shapefile_reader.py
+++ utils/shapefile/shapefile_reader.py
@@ -18,15 +18,34 @@ if __name__ == "__main__" :
     shp = open(shp_filename)
 
     header = (struct.Struct(">IIIIIII"),struct.Struct("<IIdddddddd"))
-    _,_,_,_,_,_,file_length = header[0].unpack_from(shx.read(28))
+    # SHX header
+    _,_,_,_,_,_,shx_file_length = header[0].unpack_from(shx.read(28))
     _,_,lox,loy,hix,hiy,_,_,_,_ = header[1].unpack_from(shx.read(72))
 
-    print "FILE_LENGTH=",file_length
+    # SHP header
+    _,_,_,_,_,_,shp_file_length = header[0].unpack_from(shp.read(28))
+    _,_,lox,loy,hix,hiy,_,_,_,_ = header[1].unpack_from(shp.read(72))
+
+    print "SHX FILE_LENGTH=",shx_file_length
+    print "SHP FILE_LENGTH=",shp_file_length
+
     print "BBOX(",lox,loy,hix,hiy,")"
     record_header = struct.Struct(">II")
     record = struct.Struct(">II")
-    while shx.tell() < file_length * 2 :
-        offset,_ =  record.unpack_from(shx.read(8))
+    calc_total_size = 50
+    count = 0
+    while shx.tell() < shx_file_length * 2 :
+        offset,len =  record.unpack_from(shx.read(8))
         shp.seek(offset*2, os.SEEK_SET)
-        record_number,record_length = record_header.unpack_from(shp.read(8))
-        print (2*offset),record_number,record_length
+        record_number,content_length = record_header.unpack_from(shp.read(8))
+        print (2*offset),record_number,content_length
+        calc_total_size +=(4 + content_length)
+        count+=1
+
+    print "SHAPES COUNT=",count
+
+    if shp_file_length-calc_total_size <> 0 :
+        print "BAD SHAPE FILE"
+        print "extra ", 2*(shp_file_length-calc_total_size)," bytes"
+    else:
+        print "GOOD!"
