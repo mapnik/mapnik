9453d93f1508fa0e9e7cc45ae7798a329f0d62af
artemp <artem@mapnik.org>
2012-12-14 19:22:04 +0000

    + update conversion tests to have 0.0 and negative numbers

--- tests/cpp_tests/conversions_test.cpp
+++ tests/cpp_tests/conversions_test.cpp
@@ -10,37 +10,72 @@ int main( int, char*[] )
     std::string out;
 
     // Test double
+    to_string(out, double(0));
+    BOOST_TEST_EQ( out,  "0" );
+    out.clear();
 
     to_string(out, double(1));
     BOOST_TEST_EQ( out,  "1" );
+    out.clear();
+
+    to_string(out, double(-1));
+    BOOST_TEST_EQ( out,  "-1" );
     out.clear();
 
     to_string(out, double(0.1));
     BOOST_TEST_EQ( out,  "0.1" );
     out.clear();
 
+    to_string(out, double(-0.1));
+    BOOST_TEST_EQ( out,  "-0.1" );
+    out.clear();
+
     to_string(out, double(0.123));
     BOOST_TEST_EQ( out,  "0.123" );
+    out.clear();
+
+    to_string(out, double(-0.123));
+    BOOST_TEST_EQ( out,  "-0.123" );
     out.clear();
 
     to_string(out, double(1e-06));
     BOOST_TEST_EQ( out,  "1e-06" );
     out.clear();
 
+    to_string(out, double(-1e-06));
+    BOOST_TEST_EQ( out,  "-1e-06" );
+    out.clear();
+
     to_string(out, double(1e-05));
     BOOST_TEST_EQ( out,  "0.00001" );
+    out.clear();
+
+    to_string(out, double(-1e-05));
+    BOOST_TEST_EQ( out,  "-0.00001" );
     out.clear();
 
     to_string(out, double(0.0001));
     BOOST_TEST_EQ( out,  "0.0001" );
     out.clear();
 
+    to_string(out, double(-0.0001));
+    BOOST_TEST_EQ( out,  "-0.0001" );
+    out.clear();
+
     to_string(out, double(0.0001234567890123456));
     BOOST_TEST_EQ( out,  "0.0001234567890123456" );
     out.clear();
 
+    to_string(out, double(-0.0001234567890123456));
+    BOOST_TEST_EQ( out,  "-0.0001234567890123456" );
+    out.clear();
+
     to_string(out, double(1000000000000000));
     BOOST_TEST_EQ( out,  "1000000000000000" );
+    out.clear();
+
+    to_string(out, double(-1000000000000000));
+    BOOST_TEST_EQ( out,  "-1000000000000000" );
     out.clear();
 
     to_string(out, double(100000000000000.1));
@@ -57,6 +92,10 @@ int main( int, char*[] )
 
     to_string(out, double(1.234e+16));
     BOOST_TEST_EQ( out,  "1.234e+16" );
+    out.clear();
+
+    to_string(out, double(-1.234e+16));
+    BOOST_TEST_EQ( out,  "-1.234e+16" );
     out.clear();
 
     // Test int
