a4b07553ae168be21876bc37e70919d1f491a6fd
Dane Springmeyer <dane@dbsgeo.com>
2012-12-21 20:02:03 -0800

    store copy of bbox on featureset to avoid the same potential bug as #1667 (9b3d2b7)

--- plugins/input/templates/helloworld/hello_featureset.hpp
+++ plugins/input/templates/helloworld/hello_featureset.hpp
@@ -22,7 +22,7 @@ public:
 
 private:
     // members are up to you, but these are recommended
-    mapnik::box2d<double> const& box_;
+    mapnik::box2d<double> box_;
     mapnik::value_integer feature_id_;
     boost::scoped_ptr<mapnik::transcoder> tr_;
     mapnik::context_ptr ctx_;
