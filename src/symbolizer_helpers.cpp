17b0b1eeb1ea2b15f1ff0d202b862d3a6844fea0
Dane Springmeyer <dane@dbsgeo.com>
2013-01-03 23:56:47 -0800

    iwyu

--- src/symbolizer_helpers.cpp
+++ src/symbolizer_helpers.cpp
@@ -283,7 +283,7 @@ bool text_symbolizer_helper<FaceManagerT, DetectorT>::next_placement()
     if (placement_->properties.orientation)
     {
         // https://github.com/mapnik/mapnik/issues/1352
-        mapnik::evaluate<Feature, value_type> evaluator(feature_);
+        mapnik::evaluate<feature_impl, value_type> evaluator(feature_);
         angle_ = boost::apply_visitor(
             evaluator,
             *(placement_->properties.orientation)).to_double();
