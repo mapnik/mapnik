48603669f62ca5cb7e2e039ecdc37527f2bff3cd
Dane Springmeyer <dane@dbsgeo.com>
2013-01-04 00:20:21 -0800

    remove unneeded includes

--- include/mapnik/rule.hpp
+++ include/mapnik/rule.hpp
@@ -41,9 +41,6 @@
 #include <mapnik/config.hpp> // MAPNIK_DECL
 
 // boost
-#include <boost/concept_check.hpp>
-#include <boost/shared_ptr.hpp>
-#include <boost/make_shared.hpp>
 #include <boost/variant.hpp>
 
 // stl
