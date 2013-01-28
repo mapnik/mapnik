47ab9913ee5ba6e67c7f58fd29773a20757fb0e7
Dane Springmeyer <dane@dbsgeo.com>
2013-01-28 13:48:13 -0500

    make consistent the proj4 string used for spherical mercator across tests and docs

--- bindings/python/mapnik_layer.cpp
+++ bindings/python/mapnik_layer.cpp
@@ -342,7 +342,7 @@ void export_layer()
                       "'+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs' # The default srs if not initialized with custom srs\n"
                       ">>> # set to google mercator with Proj.4 literal\n"
                       "... \n"
-                      ">>> lyr.srs = '+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +no_defs +over'\n"
+                      ">>> lyr.srs = '+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0.0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs +over'\n"
             )
 
         .add_property("group_by",
