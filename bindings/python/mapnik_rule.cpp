79f0f18f3c2b2d77218b69c4ef97505170f188b8
artemp <artem@mapnik.org>
2013-02-04 19:08:10 +0000

    + provide both symbols and copy_symbols methods

--- bindings/python/mapnik_rule.cpp
+++ bindings/python/mapnik_rule.cpp
@@ -84,6 +84,8 @@ void export_rule()
         .def("has_also",&rule::has_also_filter)
         .def("active",&rule::active)
         .add_property("symbols",make_function
+                      (&rule::get_symbolizers,return_value_policy<reference_existing_object>()))
+        .add_property("copy_symbols",make_function
                       (&rule::get_symbolizers,return_value_policy<copy_const_reference>()))
         ;
 }
