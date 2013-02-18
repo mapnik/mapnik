df7db521bb95926a13c8e3df946df59314d52126
XinGang Li <slinavlee@gmail.com>
2013-02-18 23:15:36 +0800

    Add parse_image_filters function to avoid python bindings access the internal structs.

--- src/image_filter_types.cpp
+++ src/image_filter_types.cpp
@@ -21,6 +21,7 @@
  *****************************************************************************/
 // mapnik
 #include <mapnik/image_filter_types.hpp>
+#include <mapnik/image_filter_grammar.hpp> // image_filter_grammar
 
 // boost
 #include <boost/spirit/include/karma.hpp>
@@ -61,6 +62,19 @@ bool generate_image_filters(std::back_insert_iterator<std::string>& sink, std::v
     using boost::spirit::karma::generate;
     bool r = generate(sink, stream % ' ', filters);
     return r;
+}
+
+bool parse_image_filters(std::string const& filters, std::vector<filter_type>& image_filters)
+{
+    std::string::const_iterator itr = filters.begin();
+    std::string::const_iterator end = filters.end();
+    mapnik::image_filter_grammar<std::string::const_iterator,
+                                 std::vector<mapnik::filter::filter_type> > filter_grammar;
+    bool r = boost::spirit::qi::phrase_parse(itr,end,
+                                             filter_grammar,
+                                             boost::spirit::qi::ascii::space,
+                                             image_filters);
+    return r && itr==end;
 }
 
 }}
