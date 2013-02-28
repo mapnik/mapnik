7ad2d126339952e08835e558b7cf629d12810614
artemp <artem@mapnik.org>
2013-02-28 13:59:12 -0500

    + treat .h as c++

--- utils/format_source_files/mapnik_format.el
+++ utils/format_source_files/mapnik_format.el
@@ -1,5 +1,7 @@
 (defun mapnik-format-function ()
   "Format buffer according to mapnik style (TODO)"
+  (add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
+  (c-set-style "bsd")
   (c-set-style "bsd")
   (c-set-offset 'innamespace 0)
   (c-set-offset 'template-args-cont 'c-lineup-template-args)
