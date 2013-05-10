a7a106109dbe97c93bf073e9e0a96404557d4bc3
Dane Springmeyer <dane@dbsgeo.com>
2013-05-09 17:05:39 -0700

    make mapped_memory_cache a class not struct for consistency with other singletons

--- include/mapnik/mapped_memory_cache.hpp
+++ include/mapnik/mapped_memory_cache.hpp
@@ -41,12 +41,13 @@ using namespace boost::interprocess;
 
 typedef boost::shared_ptr<mapped_region> mapped_region_ptr;
 
-struct MAPNIK_DECL mapped_memory_cache :
+class MAPNIK_DECL mapped_memory_cache :
         public singleton<mapped_memory_cache, CreateStatic>,
         private mapnik::noncopyable
 {
     friend class CreateStatic<mapped_memory_cache>;
     boost::unordered_map<std::string,mapped_region_ptr> cache_;
+public:
     bool insert(std::string const& key, mapped_region_ptr);
     boost::optional<mapped_region_ptr> find(std::string const& key, bool update_cache = false);
     void clear();
