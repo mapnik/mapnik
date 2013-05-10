7c4adea08e4523113ba03af30cafbe68244ba6ca
Dane Springmeyer <dane@dbsgeo.com>
2013-05-09 17:07:58 -0700

    support visibility attribute with gcc/clang - refs #1826

--- include/mapnik/config.hpp
+++ include/mapnik/config.hpp
@@ -39,9 +39,15 @@
 #    pragma warning(disable : 4996) //_CRT_SECURE_NO_DEPRECATE
 #  endif
 #else
+#  if __GNUC__ >= 4
+#  define MAPNIK_EXP __attribute__ ((visibility ("default")))
+#  define MAPNIK_DECL __attribute__ ((visibility ("default")))
+#  define MAPNIK_IMP __attribute__ ((visibility ("default")))
+#  else
 #  define MAPNIK_EXP
-#  define MAPNIK_IMP
 #  define MAPNIK_DECL
+#  define MAPNIK_IMP
+#  endif
 #endif
 
 #define PROJ_ENVELOPE_POINTS 20
