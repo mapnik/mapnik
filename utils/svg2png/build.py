2533e8137fa5b1e15e0422257b1f708919cd3f1f
Dane Springmeyer <dane@dbsgeo.com>
2013-07-14 15:28:22 -0400

    fix syntax

--- utils/svg2png/build.py
+++ utils/svg2png/build.py
@@ -39,9 +39,9 @@ if env['HAS_CAIRO']:
     program_env.PrependUnique(CPPPATH=env['CAIRO_CPPPATHS'])
     program_env.Append(CPPDEFINES = '-DHAVE_CAIRO')
 
+boost_program_options = 'boost_program_options%s' % env['BOOST_APPEND']
 libraries = ['mapnik',boost_program_options]
 libraries.extend(copy(env['LIBMAPNIK_LIBS']))
-boost_program_options = 'boost_program_options%s' % env['BOOST_APPEND']
 if env['RUNTIME_LINK'] == 'static' and env['PLATFORM'] == 'Linux':
     libraries.append('dl')
 
