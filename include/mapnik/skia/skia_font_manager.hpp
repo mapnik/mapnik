5d910485347d8deab0c30a621b7702b4a68b8195
artemp <artem@mapnik.org>
2013-07-17 18:31:21 +0100

    + add missing header (dummy impl atm)

---
+++ include/mapnik/skia/skia_font_manager.hpp
@@ -0,0 +1,46 @@
+/*****************************************************************************
+ *
+ * This file is part of Mapnik (c++ mapping toolkit)
+ *
+ * Copyright (C) 2013 Artem Pavlenko
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ *****************************************************************************/
+
+
+#if defined(HAVE_SKIA)
+
+#ifndef MAPNIK_SKIA_FONT_MANAGER_HPP
+#define MAPNIK_SKIA_FONT_MANAGER_HPP
+
+#include <iostream>
+namespace mapnik
+{
+
+
+class skia_font_manager
+{
+public:
+    void test() const
+    {
+        std::cerr << "skia_font_manager" << std::endl;
+    }
+};
+
+}
+
+#endif // MAPNIK_SKIA_FONT_MANAGER_HPP
+#endif // #if defined(HAVE_SKIA)
