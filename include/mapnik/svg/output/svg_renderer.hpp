a9627f835b08a893cc40f2ed9c98d6f97d3c591a
Matt Amos <zerebubuth@gmail.com>
2013-12-06 14:14:14 +0000

    Factored common renderer code from SVG renderer.

--- include/mapnik/svg/output/svg_renderer.hpp
+++ include/mapnik/svg/output/svg_renderer.hpp
@@ -37,6 +37,7 @@
 #include <mapnik/image_compositing.hpp>  // for composite_mode_e
 #include <mapnik/pixel_position.hpp>
 #include <mapnik/request.hpp>
+#include <mapnik/renderer_common.hpp>
 
 // boost
 #include <boost/variant/static_visitor.hpp>
@@ -154,17 +155,10 @@ public:
 
 private:
     OutputIterator& output_iterator_;
-    const int width_;
-    const int height_;
-    double scale_factor_;
-    CoordTransform t_;
     svg::path_output_attributes path_attributes_;
-    freetype_engine font_engine_;
-    face_manager<freetype_engine> font_manager_;
-    std::shared_ptr<label_collision_detector4> detector_;
     svg::svg_generator<OutputIterator> generator_;
-    box2d<double> query_extent_;
     bool painted_;
+    renderer_common common_;
 
     /*!
      * @brief Visitor that makes the calls to process each symbolizer when stored in a boost::variant.
