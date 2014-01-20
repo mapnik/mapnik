5153678876da7a4d883f0f31f99a243206251423
Dane Springmeyer <dane@dbsgeo.com>
2014-01-20 11:07:18 -0800

    fix linking order in svg c++ tests
    Conflicts:
    	tests/cpp_tests/svg_renderer_tests/build.py

--- tests/cpp_tests/svg_renderer_tests/build.py
+++ tests/cpp_tests/svg_renderer_tests/build.py
@@ -4,15 +4,9 @@ from copy import copy
 
 Import ('env')
 
-headers = env['CPPPATH'] 
-
-filesystem = 'boost_filesystem%s' % env['BOOST_APPEND']
-system = 'boost_system%s' % env['BOOST_APPEND']
-regex = 'boost_regex%s' % env['BOOST_APPEND']
-
-libraries =  copy(env['LIBMAPNIK_LIBS'])
-libraries.append('mapnik')
+libraries = ['mapnik']
+libraries.extend(copy(env['LIBMAPNIK_LIBS']))
 
 for cpp_test in glob.glob('*_test.cpp'):
-    test_program = env.Program(cpp_test.replace('.cpp',''), [cpp_test], CPPPATH=headers, LIBS=libraries, LINKFLAGS=env['CUSTOM_LDFLAGS'])
+    test_program = env.Program(cpp_test.replace('.cpp',''), [cpp_test], LIBS=libraries)
     Depends(test_program, env.subst('../../../src/%s' % env['MAPNIK_LIB_NAME']))
