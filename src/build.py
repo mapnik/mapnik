cd4c645032211dd9d338182c430936e926a54672
Dane Springmeyer <dane@dbsgeo.com>
2014-01-23 00:42:05 -0800

    boost threads are no longer needed

--- src/build.py
+++ src/build.py
@@ -56,14 +56,10 @@ enabled_imaging_libraries = []
 filesystem = 'boost_filesystem%s' % env['BOOST_APPEND']
 regex = 'boost_regex%s' % env['BOOST_APPEND']
 system = 'boost_system%s' % env['BOOST_APPEND']
-thread = 'boost_thread%s' % env['BOOST_APPEND']
 
 # clear out and re-set libs for this env
 # note: order matters on linux: see lorder | tsort
-lib_env['LIBS'] = [filesystem,
-                   regex,
-                   thread
-                  ]
+lib_env['LIBS'] = [filesystem,regex]
 
 if env['HAS_CAIRO']:
     lib_env.Append(LIBS=env['CAIRO_ALL_LIBS'])
