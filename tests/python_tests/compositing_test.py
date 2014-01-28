5083f3a30e46fecbf14ac20cec7b495bda68a5ce
Dane Springmeyer <dane@dbsgeo.com>
2014-01-27 16:28:34 -0800

    more expected image test updates

--- tests/python_tests/compositing_test.py
+++ tests/python_tests/compositing_test.py
@@ -92,10 +92,10 @@ def test_compare_images():
         a.demultiply()
         if not validate_pixels_are_not_premultiplied(a):
             fails.append('%s not validly demultiplied' % (name))
-        a.save(actual)
+        a.save(actual,'png32')
         if not os.path.exists(expected):
             print 'generating expected test image: %s' % expected
-            a.save(expected)
+            a.save(expected,'png32')
         expected_im = mapnik.Image.open(expected)
         # compare them
         if a.tostring('png32') == expected_im.tostring('png32'):
@@ -103,7 +103,7 @@ def test_compare_images():
         else:
             fails.append('failed comparing actual (%s) and expected(%s)' % (actual,'tests/python_tests/'+ expected))
             fail_im = side_by_side_image(expected_im, a)
-            fail_im.save('/tmp/mapnik-comp-op-test-' + name + '.fail.png')
+            fail_im.save('/tmp/mapnik-comp-op-test-' + name + '.fail.png','png32')
     eq_(len(successes),num_ops,'\n'+'\n'.join(fails))
     b.demultiply()
     # b will be slightly modified by pre and then de multiplication rounding errors
@@ -166,10 +166,10 @@ if 'shape' in mapnik.DatasourceCache.plugin_names():
             mapnik.render(m, im)
             actual = '/tmp/mapnik-style-comp-op-' + name + '.png'
             expected = 'images/style-comp-op/' + name + '.png'
-            im.save(actual)
+            im.save(actual,'png32')
             if not os.path.exists(expected):
                 print 'generating expected test image: %s' % expected
-                im.save(expected)
+                im.save(expected,'png32')
             expected_im = mapnik.Image.open(expected)
             # compare them
             if im.tostring('png32') == expected_im.tostring('png32'):
@@ -177,7 +177,7 @@ if 'shape' in mapnik.DatasourceCache.plugin_names():
             else:
                 fails.append('failed comparing actual (%s) and expected(%s)' % (actual,'tests/python_tests/'+ expected))
                 fail_im = side_by_side_image(expected_im, im)
-                fail_im.save('/tmp/mapnik-style-comp-op-' + name + '.fail.png')
+                fail_im.save('/tmp/mapnik-style-comp-op-' + name + '.fail.png','png32')
         eq_(len(fails), 0, '\n'+'\n'.join(fails))
 
     def test_style_level_opacity():
@@ -188,7 +188,7 @@ if 'shape' in mapnik.DatasourceCache.plugin_names():
         mapnik.render(m,im)
         actual = '/tmp/mapnik-style-level-opacity.png'
         expected = 'images/support/mapnik-style-level-opacity.png'
-        im.save(actual)
+        im.save(actual,'png32')
         expected_im = mapnik.Image.open(expected)
         eq_(im.tostring('png32'),expected_im.tostring('png32'), 'failed comparing actual (%s) and expected (%s)' % (actual,'tests/python_tests/'+ expected))
 
