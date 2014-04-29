59bba65ce808dd7e3b75fea964460feda7dd1d0f
Dane Springmeyer <dane@mapbox.com>
2014-04-28 20:17:38 -0700

    inherit PYTHONPATH in localize.sh

--- localize.sh
+++ localize.sh
@@ -6,7 +6,7 @@ if [ ${UNAME} = 'Darwin' ]; then
 else
     export LD_LIBRARY_PATH="${CURRENT_DIR}/src/"
 fi
-export PYTHONPATH="${CURRENT_DIR}/bindings/python/"
+export PYTHONPATH="${CURRENT_DIR}/bindings/python/":$PYTHONPATH
 export MAPNIK_FONT_DIRECTORY="${CURRENT_DIR}/fonts/dejavu-fonts-ttf-2.33/ttf/"
 export MAPNIK_INPUT_PLUGINS_DIRECTORY="${CURRENT_DIR}/plugins/input/"
 export PATH="${CURRENT_DIR}/bin/":${PATH}
