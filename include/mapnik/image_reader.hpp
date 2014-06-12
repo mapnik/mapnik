8c5049e0f87e6e3e531a49d58ef0896706bdf970
Dane Springmeyer <dane@mapbox.com>
2014-06-11 18:50:58 -0700

    image readers: ability to check if image has alpha before full read

--- include/mapnik/image_reader.hpp
+++ include/mapnik/image_reader.hpp
@@ -57,6 +57,7 @@ struct MAPNIK_DECL image_reader : private mapnik::noncopyable
 {
     virtual unsigned width() const=0;
     virtual unsigned height() const=0;
+    virtual bool has_alpha() const=0;
     virtual bool premultiplied_alpha() const=0;
     virtual void read(unsigned x,unsigned y,image_data_32& image)=0;
     virtual ~image_reader() {}
