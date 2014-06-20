6ce0828a6f9c74566c4c5f485fabff3704d1c86e
Dane Springmeyer <dane@mapbox.com>
2014-06-19 17:21:57 -0700

    font registration test

---
+++ benchmark/test_font_registration.cpp
@@ -0,0 +1,26 @@
+#include "bench_framework.hpp"
+#include <mapnik/font_engine_freetype.hpp>
+#include <boost/format.hpp>
+#include <boost/foreach.hpp>
+
+class test : public benchmark::test_case
+{
+public:
+    test(mapnik::parameters const& params)
+     : test_case(params) {}
+    bool validate() const
+    {
+        return mapnik::freetype_engine::register_fonts("./fonts", true);
+    }
+    void operator()() const
+    {
+        unsigned long count = 0;
+        for (unsigned i=0;i<iterations_;++i)
+        {
+            mapnik::freetype_engine::register_fonts("./fonts", true);
+            count++;
+        }
+    }
+};
+
+BENCHMARK(test,"font registration")
