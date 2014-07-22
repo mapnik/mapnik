8301e28b13ab1a0900db7f56de88805caedb1390
Dane Springmeyer <dane@mapbox.com>
2014-07-22 12:17:40 -0700

    more marker tests on polygons - able to detect change from https://github.com/mapnik/mapnik/commit/ecc5acbdb953e172fcc652b55ed19b8b581e2146

--- tests/visual_tests/test.py
+++ tests/visual_tests/test.py
@@ -244,7 +244,8 @@ files = {
     'text-halo-opacity':{'sizes':[(512,512)]},
     # https://github.com/mapnik/mapnik/issues/2202
     'line-smooth-and-offset':{'sizes':[(512,512)]},
-    'halo-comp-op-on-satellite':{'sizes':[(450,450)]}
+    'halo-comp-op-on-satellite':{'sizes':[(450,450)]},
+    'marker-whole-multi-polygon':{'sizes':[(512,512)]}
     }
 
 class Reporting:
