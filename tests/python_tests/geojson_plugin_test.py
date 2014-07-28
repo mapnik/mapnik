ddda3dc56d96fe9c4f461e96cb01c53964b1f31c
Dane Springmeyer <dane@mapbox.com>
2014-07-28 13:07:27 -0700

    fix geojson plugin test

--- tests/python_tests/geojson_plugin_test.py
+++ tests/python_tests/geojson_plugin_test.py
@@ -22,20 +22,20 @@ if 'geojson' in mapnik.DatasourceCache.plugin_names():
 
     def test_geojson_properties():
         ds = mapnik.Datasource(type='geojson',file='../data/json/escaped.geojson')
-        f = ds.features_at_point(s.envelope().center()).features[0]
+        f = ds.features_at_point(ds.envelope().center()).features[0]
 
         desc = ds.describe()
         eq_(desc['geometry_type'],mapnik.DataGeometryType.Point)
 
-        eq_(f['name'], u'test')
-        eq_(f['description'], u'Test: \u005C')
+        eq_(f['name'], u'Test')
         eq_(f['int'], 1)
-        eq_(f['double'], u'Quebec')
+        eq_(f['description'], u'Test: \u005C')
+        eq_(f['spaces'], u'this has spaces')
+        eq_(f['double'], 1.1)
         eq_(f['boolean'], True)
         eq_(f['NOM_FR'], u'Qu\xe9bec')
         eq_(f['NOM_FR'], u'Québec')
 
-    def test_geojson_properties():
         ds = mapnik.Datasource(type='geojson',file='../data/json/escaped.geojson')
         f = ds.all_features()[0]
 
@@ -44,17 +44,17 @@ if 'geojson' in mapnik.DatasourceCache.plugin_names():
 
         eq_(f['name'], u'Test')
         eq_(f['int'], 1)
+        eq_(f['description'], u'Test: \u005C')
+        eq_(f['spaces'], u'this has spaces')
         eq_(f['double'], 1.1)
         eq_(f['boolean'], True)
         eq_(f['NOM_FR'], u'Qu\xe9bec')
         eq_(f['NOM_FR'], u'Québec')
-        eq_(f['spaces'], u'this has spaces')
-        eq_(f['description'], u'Test: \u005C')
 
 #    @raises(RuntimeError)
     def test_that_nonexistant_query_field_throws(**kwargs):
         ds = mapnik.Datasource(type='geojson',file='../data/json/escaped.geojson')
-        eq_(len(ds.fields()),7)
+        eq_(len(ds.fields()),8)
         # TODO - this sorting is messed up
         #eq_(ds.fields(),['name', 'int', 'double', 'description', 'boolean', 'NOM_FR'])
         #eq_(ds.field_types(),['str', 'int', 'float', 'str', 'bool', 'str'])
