cdefc71488453e27819ad2dbc6452a9f9ac97c92
Dane Springmeyer <dane@mapbox.com>
2014-09-04 16:40:28 -0700

    Revert "start fixing visual tests to run on windows"
    This reverts commit 79c6ab46a5d1d627dae495be8fd86df2f0ddbbf7.

--- tests/visual_tests/compare.py
+++ tests/visual_tests/compare.py
@@ -3,7 +3,6 @@
 import os
 import sys
 import mapnik
-import platform
 
 try:
     import json
@@ -14,7 +13,7 @@ COMPUTE_THRESHOLD = 16
 
 # testcase images are generated on OS X
 # so they should exactly match
-if platform.uname()[0] == 'Darwin':
+if os.uname()[0] == 'Darwin':
     COMPUTE_THRESHOLD = 2
 
 # returns true if pixels are not identical
--- tests/visual_tests/test.py
+++ tests/visual_tests/test.py
@@ -7,7 +7,6 @@ import mapnik
 #mapnik.logger.set_severity(mapnik.severity_type.Debug)
 import shutil
 import os.path
-import platform
 from compare import compare, compare_grids
 
 try:
@@ -27,8 +26,7 @@ defaults = {
 
 cairo_threshold = 10
 agg_threshold = 0
-UNAME = platform.uname()[0]
-if 'Linux' == UNAME or 'Windows' == UNAME:
+if 'Linux' == os.uname()[0]:
     # we assume if linux then you are running packaged cairo
     # which is older than the 1.12.14 version we used on OS X
     # to generate the expected images, so we'll rachet back the threshold
@@ -294,9 +292,9 @@ class Reporting:
     def result_fail(self, actual, expected, diff):
         self.failed += 1
         if self.quiet:
-            sys.stderr.write('x')
+            sys.stderr.write('\x1b[31m.\x1b[0m')
         else:
-            print 'x[34m%u different pixels\x1b[0m)' % diff
+            print '\x1b[31m✘\x1b[0m (\x1b[34m%u different pixels\x1b[0m)' % diff
 
         if self.overwrite_failures:
             self.errors.append((self.REPLACE, actual, expected, diff, None))
@@ -308,17 +306,17 @@ class Reporting:
     def result_pass(self, actual, expected, diff):
         self.passed += 1
         if self.quiet:
-            sys.stderr.write('.')
+            sys.stderr.write('\x1b[32m.\x1b[0m')
         else:
-            print '.'
+            print '\x1b[32m✓\x1b[0m'
 
     def not_found(self, actual, expected):
         self.failed += 1
         self.errors.append((self.NOT_FOUND, actual, expected, 0, None))
         if self.quiet:
-            sys.stderr.write('x')
+            sys.stderr.write('\x1b[33m.\x1b[0m')
         else:
-            print 'x (\x1b[34mReference file not found, creating\x1b[0m)'
+            print '\x1b[33m?\x1b[0m (\x1b[34mReference file not found, creating\x1b[0m)'
         contents = open(actual, 'r').read()
         open(expected, 'wb').write(contents)
 
@@ -326,9 +324,9 @@ class Reporting:
         self.failed += 1
         self.errors.append((self.OTHER, None, expected, 0, message))
         if self.quiet:
-            sys.stderr.write('x')
+            sys.stderr.write('\x1b[31m.\x1b[0m')
         else:
-            print 'x([%s)' % message
+            print '\x1b[31m✘\x1b[0m (\x1b[34m%s\x1b[0m)' % message
 
     def make_html_item(self,actual,expected,diff):
         item = '''
