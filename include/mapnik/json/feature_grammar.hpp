1382e57ebe1e865c7c87bf8712c9f9f77517e10a
artemp <artem@mapnik.org>
2014-09-23 14:02:08 +0100

    avoid allocating potentially large std::string in error_handler

--- include/mapnik/json/feature_grammar.hpp
+++ include/mapnik/json/feature_grammar.hpp
@@ -97,7 +97,7 @@ struct error_handler
         Iterator first, Iterator last,
         Iterator err_pos, boost::spirit::info const& what) const
     {
-        MAPNIK_LOG_ERROR(error_handler) << what << " expected but got: " << where_message()(err_pos, last, 16);
+        MAPNIK_LOG_ERROR(error_handler) << what << " expected but got: " << std::string(err_pos, std::min(err_pos + 16,last));
     }
 };
 
