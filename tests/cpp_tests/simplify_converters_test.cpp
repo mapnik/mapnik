d8a9a5ed4d986fe62e90033d1d248afc1e5ce9eb
Kevin Kreiser <kevinkreiser@gmail.com>
2014-10-08 14:33:50 -0400

    make the simplify test function general purpose so others can add other test cases for other simplification algorithms

--- tests/cpp_tests/simplify_converters_test.cpp
+++ tests/cpp_tests/simplify_converters_test.cpp
@@ -16,8 +16,8 @@
 // stl
 #include <stdexcept>
 
-// Ramer Douglas Peucker generalization
-void ramer_douglas_peucker(const std::string& wkt_in, double tolerance, const std::string& expected)
+// Convenience method for test cases
+void simplify(const std::string& wkt_in, double tolerance, const std::string& method, const std::string& expected)
 {
     //grab the geom
     mapnik::geometry_container multi_input;
@@ -27,7 +27,7 @@ void ramer_douglas_peucker(const std::string& wkt_in, double tolerance, const st
     }
     //setup the generalization
     mapnik::simplify_converter<mapnik::geometry_type> generalizer(multi_input.front());
-    generalizer.set_simplify_algorithm(mapnik::simplify_algorithm_from_string("douglas-peucker").get());
+    generalizer.set_simplify_algorithm(mapnik::simplify_algorithm_from_string(method).get());
     generalizer.set_simplify_tolerance(tolerance);
     //suck the vertices back out of it
     mapnik::geometry_type* output = new mapnik::geometry_type(multi_input.front().type());
@@ -56,33 +56,33 @@ int main(int argc, char** argv)
 
     BOOST_TEST(set_working_dir(args));
 
-    ramer_douglas_peucker(  std::string("LineString(0 0,2 2,3 5,4 1,5 0,6 7,7 0)"),
-                            4,
-                            std::string("LineString(0 0,6 7,7 0)"));
+    simplify(	std::string("LineString(0 0,2 2,3 5,4 1,5 0,6 7,7 0)"),
+				4, "douglas-peucker",
+				std::string("LineString(0 0,6 7,7 0)"));
 
-    ramer_douglas_peucker(  std::string("LineString(0 0,2 2,3 5,4 1,5 0,6 7,7 0)"),
-                            2,
-                            std::string("LineString(0 0,3 5,5 0,6 7,7 0)"));
+    simplify(  	std::string("LineString(0 0,2 2,3 5,4 1,5 0,6 7,7 0)"),
+				2, "douglas-peucker",
+				std::string("LineString(0 0,3 5,5 0,6 7,7 0)"));
 
-    ramer_douglas_peucker(  std::string("LineString(10 0,9 -4,7 -7,4 -9,0 -10,-4 -9,-7 -7,-9 -4,-10 0,-9 4,-7 7,-4 9,0 10,4 9,7 7,9 4)"),
-                            4,
-                            std::string("LineString(10 0,0 -10,-10 0,0 10,9 4)"));
+    simplify(  	std::string("LineString(10 0,9 -4,7 -7,4 -9,0 -10,-4 -9,-7 -7,-9 -4,-10 0,-9 4,-7 7,-4 9,0 10,4 9,7 7,9 4)"),
+				4, "douglas-peucker",
+				std::string("LineString(10 0,0 -10,-10 0,0 10,9 4)"));
 
-    ramer_douglas_peucker(  std::string("LineString(0 0,1 1,2 2,0 10,0 0)"),
-                            10,
-                            std::string("LineString(0 0,0 0)"));
+	simplify(  	std::string("LineString(0 0,1 1,2 2,0 10,0 0)"),
+				10, "douglas-peucker",
+				std::string("LineString(0 0,0 0)"));
 
-    ramer_douglas_peucker(  std::string("LineString(0 0,1 1,2 2,0 10,0 0)"),
-                            8,
-                            std::string("LineString(0 0,0 10,0 0)"));
+    simplify(  	std::string("LineString(0 0,1 1,2 2,0 10,0 0)"),
+				8, "douglas-peucker",
+				std::string("LineString(0 0,0 10,0 0)"));
 
-    ramer_douglas_peucker(  std::string("LineString(0 0,1 1,2 2,0 10,0 0)"),
-                            1,
-                            std::string("LineString(0 0,2 2,0 10,0 0)"));
+    simplify(  	std::string("LineString(0 0,1 1,2 2,0 10,0 0)"),
+				1, "douglas-peucker",
+				std::string("LineString(0 0,2 2,0 10,0 0)"));
 
-    ramer_douglas_peucker(  std::string("LineString(0 0, 1 -1, 2 2, 0 -10, 0 0, -5 7, 4 6)"),
-                            3,
-                            std::string("LineString(0 0,0 -10,-5 7,4 6)"));
+    simplify(  	std::string("LineString(0 0, 1 -1, 2 2, 0 -10, 0 0, -5 7, 4 6)"),
+				3, "douglas-peucker",
+				std::string("LineString(0 0,0 -10,-5 7,4 6)"));
 
     if (!::boost::detail::test_errors())
     {
