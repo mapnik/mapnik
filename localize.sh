900def16ae3c7e9ddf35decccb6e5de1a79aa064
Dane Springmeyer <dane@mapbox.com>
2014-10-23 00:06:40 -0700

    put mapnik-config on PATH [skip ci]

--- localize.sh
+++ localize.sh
@@ -9,5 +9,5 @@ fi
 export PYTHONPATH="${CURRENT_DIR}/bindings/python/":$PYTHONPATH
 export MAPNIK_FONT_DIRECTORY="${CURRENT_DIR}/fonts/dejavu-fonts-ttf-2.34/ttf/"
 export MAPNIK_INPUT_PLUGINS_DIRECTORY="${CURRENT_DIR}/plugins/input/"
-export PATH="${CURRENT_DIR}/bin/":${PATH}
+export PATH="${CURRENT_DIR}/utils/mapnik-config":${PATH}
 export PATH="${CURRENT_DIR}/utils/nik2img":${PATH}
