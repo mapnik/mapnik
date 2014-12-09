dc1ed0ae2981c89fd1540872b6c9bd3cf8c42ec8
Dane Springmeyer <dane@mapbox.com>
2014-12-08 22:58:44 -0500

    tiff io: also test reading from in memory buffer

--- tests/cxx/tiff_io.cpp
+++ tests/cxx/tiff_io.cpp
@@ -6,30 +6,40 @@
 #include <mapnik/tiff_io.hpp>
 #include "../../src/tiff_reader.cpp"
 
-std::unique_ptr<mapnik::image_reader> open(std::string const& filename)
-{
-    return std::unique_ptr<mapnik::image_reader>(mapnik::get_image_reader(filename,"tiff")); 
-}
-
 #define TIFF_ASSERT(filename) \
     mapnik::tiff_reader<boost::iostreams::file_source> tiff_reader(filename); \
     REQUIRE( tiff_reader.width() == 256 ); \
     REQUIRE( tiff_reader.height() == 256 ); \
-    auto reader = open(filename); \
+    std::unique_ptr<mapnik::image_reader> reader(mapnik::get_image_reader(filename,"tiff")); \
     REQUIRE( reader->width() == 256 ); \
     REQUIRE( reader->height() == 256 ); \
+    mapnik::util::file file(filename); \
+    mapnik::tiff_reader<boost::iostreams::array_source> tiff_reader2(file.data().get(),file.size()); \
+    REQUIRE( tiff_reader2.width() == 256 ); \
+    REQUIRE( tiff_reader2.height() == 256 ); \
+    std::unique_ptr<mapnik::image_reader> reader2(mapnik::get_image_reader(file.data().get(),file.size())); \
+    REQUIRE( reader2->width() == 256 ); \
+    REQUIRE( reader2->height() == 256 ); \
 
 #define TIFF_ASSERT_ALPHA \
     REQUIRE( tiff_reader.has_alpha() == true ); \
     REQUIRE( tiff_reader.premultiplied_alpha() == false ); \
     REQUIRE( reader->has_alpha() == true ); \
     REQUIRE( reader->premultiplied_alpha() == false ); \
+    REQUIRE( tiff_reader2.has_alpha() == true ); \
+    REQUIRE( tiff_reader2.premultiplied_alpha() == false ); \
+    REQUIRE( reader2->has_alpha() == true ); \
+    REQUIRE( reader2->premultiplied_alpha() == false ); \
 
 #define TIFF_ASSERT_NO_ALPHA \
     REQUIRE( tiff_reader.has_alpha() == false ); \
     REQUIRE( tiff_reader.premultiplied_alpha() == false ); \
     REQUIRE( reader->has_alpha() == false ); \
     REQUIRE( reader->premultiplied_alpha() == false ); \
+    REQUIRE( tiff_reader2.has_alpha() == false ); \
+    REQUIRE( tiff_reader2.premultiplied_alpha() == false ); \
+    REQUIRE( reader2->has_alpha() == false ); \
+    REQUIRE( reader2->premultiplied_alpha() == false ); \
 
 #define TIFF_ASSERT_SIZE( data,reader ) \
     REQUIRE( data.width() == reader->width() ); \
