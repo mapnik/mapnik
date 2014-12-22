f1373883c290d12df25ea9e23b2f395832d1eb2e
artemp <artem@mapnik.org>
2014-12-22 11:52:13 +0100

    json feature_generator_grammar - re-factor properties_generator_grammar into separate compilation unit - ref #2546

--- include/mapnik/json/feature_generator_grammar.hpp
+++ include/mapnik/json/feature_generator_grammar.hpp
@@ -24,23 +24,13 @@
 #define MAPNIK_JSON_FEATURE_GENERATOR_GRAMMAR_HPP
 
 // mapnik
-#include <mapnik/value_types.hpp>
-#include <mapnik/value.hpp>
 #include <mapnik/feature.hpp>
 #include <mapnik/geometry_container.hpp>
 #include <mapnik/json/geometry_generator_grammar.hpp>
-#include <mapnik/feature_kv_iterator.hpp>
+#include <mapnik/json/properties_generator_grammar.hpp>
 
 // boost
 #include <boost/spirit/include/karma.hpp>
-#include <boost/spirit/include/phoenix.hpp>
-#include <boost/spirit/include/phoenix_function.hpp>
-#include <boost/spirit/include/phoenix_fusion.hpp>
-#include <boost/spirit/include/phoenix_core.hpp>
-#include <boost/spirit/include/phoenix_operator.hpp>
-#include <boost/fusion/adapted/std_tuple.hpp>
-#include <boost/fusion/include/at.hpp>
-#include <boost/fusion/include/cons.hpp>
 
 namespace boost { namespace spirit { namespace traits {
 
@@ -101,73 +91,15 @@ struct get_id
     }
 };
 
-struct make_properties_range
-{
-    using properties_range_type = boost::iterator_range<mapnik::feature_kv_iterator>;
-
-    template <typename T>
-    struct result { using type = properties_range_type; };
-
-    properties_range_type operator() (mapnik::feature_impl const& f) const
-    {
-        return boost::make_iterator_range(f.begin(),f.end());
-    }
-};
-
-struct utf8
-{
-    template <typename T>
-    struct result { using type = std::string; };
-
-    std::string operator() (mapnik::value_unicode_string const& ustr) const
-    {
-        std::string result;
-        to_utf8(ustr,result);
-        return result;
-    }
-};
-
 template <typename OutputIterator>
-struct escaped_string
-    : karma::grammar<OutputIterator, std::string(char const*)>
-{
-    escaped_string();
-    karma::rule<OutputIterator, std::string(char const*)> esc_str;
-    karma::symbols<char, char const*> esc_char;
-};
-
-struct extract_string
-{
-    using result_type = std::tuple<std::string,bool>;
-
-    result_type operator() (mapnik::value const& val) const
-    {
-        bool need_quotes = val.is<value_unicode_string>();
-        return std::make_tuple(val.to_string(), need_quotes);
-    }
-};
-
-template <typename OutputIterator>
-struct feature_generator_grammar:
+struct feature_generator_grammar :
         karma::grammar<OutputIterator, mapnik::feature_impl const&()>
 {
-    using pair_type = std::tuple<std::string, mapnik::value>;
-    using range_type = make_properties_range::properties_range_type;
-
     feature_generator_grammar();
     karma::rule<OutputIterator, mapnik::feature_impl const&()> feature;
     multi_geometry_generator_grammar<OutputIterator, mapnik::geometry_container> geometry;
-    escaped_string<OutputIterator> escaped_string_;
-    karma::rule<OutputIterator, mapnik::feature_impl const&()> properties;
-    karma::rule<OutputIterator, pair_type()> pair;
-    karma::rule<OutputIterator, std::tuple<std::string,bool>()> value;
-    karma::rule<OutputIterator, mapnik::value_null()> value_null_;
-    karma::rule<OutputIterator, mapnik::value_unicode_string()> ustring;
-    typename karma::int_generator<mapnik::value_integer,10, false> int__;
+    properties_generator_grammar<OutputIterator, mapnik::feature_impl> properties;
     boost::phoenix::function<get_id> id_;
-    boost::phoenix::function<extract_string> extract_string_;
-    boost::phoenix::function<utf8> utf8_;
-    std::string quote_;
 };
 
 }}
