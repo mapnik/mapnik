76df4a2117a73a4a78a060e6ec921356bb82a3ee
artemp <artem@mapnik.org>
2015-01-27 15:50:32 +0100

    move operator<< into separate header <mapnik/util/variant_io.hpp>

--- include/mapnik/util/variant.hpp
+++ include/mapnik/util/variant.hpp
@@ -523,24 +523,6 @@ private:
     Variant const& lhs_;
 };
 
-// operator<< helper
-template <typename Out>
-class printer
-{
-public:
-    explicit printer(Out & out)
-        : out_(out) {}
-    printer& operator=(printer const&) = delete;
-
-// visitor
-    template <typename T>
-    void operator()(T const& operand) const
-    {
-        out_ << operand;
-    }
-private:
-    Out & out_;
-};
 
 } // namespace detail
 
@@ -798,15 +780,6 @@ ResultType const&  get(T const& var)
 }
 
 
-// operator<<
-template <typename charT, typename traits, typename... Types>
-VARIANT_INLINE std::basic_ostream<charT, traits>&
-operator<< (std::basic_ostream<charT, traits>& out, variant<Types...> const& rhs)
-{
-    detail::printer<std::basic_ostream<charT, traits>> visitor(out);
-    apply_visitor(visitor, rhs);
-    return out;
-}
 
 }}
 
