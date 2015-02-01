0d6c10a2faad033e1c769238e064d2228704739f
Dane Springmeyer <dane@mapbox.com>
2015-02-01 09:59:12 -0800

    fix gamma misuage in image_encoding_speed_test

--- tests/python_tests/image_encoding_speed_test.py
+++ tests/python_tests/image_encoding_speed_test.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import sys
 import os, mapnik
 from timeit import Timer, time
-from nose.tools import *
 from utilities import execution_path, run_all
 
 def setup():
@@ -42,8 +40,8 @@ combinations = ['png',
                 'png:z=1:s=filtered',
                 'png:z=1:s=huff',
                 'png:z=1:s=rle',
-                'png:m=h:g=2.0',
-                'png:m=h:g=1.0',
+                'png8:m=h:g=2.0',
+                'png8:m=h:g=1.0',
                 'png:e=miniz',
                 'png8:e=miniz'
                ]
@@ -59,7 +57,7 @@ iterations = 10
 
 def do_encoding():
 
-    image = None
+    global image
 
     results = {}
     sortable = {}
