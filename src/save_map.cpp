189cd4429f67e82259105b75a2a66866bfd1bfd4
Walid Ibrahim <walid.saad@yahoo.com>
2015-02-05 16:04:43 -0500

    added include header removed accidentally during merge

--- src/save_map.cpp
+++ src/save_map.cpp
@@ -47,6 +47,7 @@
 #include <mapnik/group/group_layout.hpp>
 #include <mapnik/group/group_symbolizer_properties.hpp>
 #include <mapnik/util/variant.hpp>
+#include <mapnik/util/variant_io.hpp>
 
 // boost
 #pragma GCC diagnostic push
