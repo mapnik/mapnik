2a8174831a377a4b84f2494f0e7a6119138eaeb1
artemp <artem@mapnik.org>
2015-02-20 17:44:35 +0100

    mapnik-geometry - simplify and streamline implementation

--- include/mapnik/geometry_impl.hpp
+++ include/mapnik/geometry_impl.hpp
@@ -83,64 +83,20 @@ struct bounding_box
     point p1;
 };
 
-struct vertex_sequence
+struct line_string : std::vector<point>
 {
-    typedef std::vector<point> cont_type;
-    cont_type data;
-    void reserve(std::size_t size)
-    {
-        data.reserve(size);
-    }
-};
-
-struct line_string : vertex_sequence
-{
-    using const_iterator_type = cont_type::const_iterator;
-    using iterator_type = cont_type::iterator;
-    using value_type = cont_type::value_type;
-    iterator_type begin() { return data.begin(); }
-    iterator_type end() { return data.end(); }
-    const_iterator_type begin() const { return data.begin(); }
-    const_iterator_type end() const { return data.end(); }
     line_string() = default;
     line_string (line_string && other) = default ;
     line_string& operator=(line_string &&) = default;
     line_string (line_string const& ) = delete;
     line_string& operator=(line_string const&) = delete;
-    inline std::size_t num_points() const { return data.size(); }
-    inline std::size_t size() const { return data.size(); } // TODO - remove
-
-    inline void clear() { data.clear();}
-    inline void resize(std::size_t new_size) { data.resize(new_size);}
-    inline void push_back(value_type const& val) { data.push_back(val);}
-    void add_coord(double x, double y)
-    {
-        data.emplace_back(x,y);
-    }
-    void emplace_back(double x, double y) // TODO - remove
-    {
-        data.emplace_back(x,y);
-    }
+    inline std::size_t num_points() const { return size(); }
+    inline void add_coord(double x, double y) { emplace_back(x,y);}
 };
 
-struct polygon2
-{
-    //polygon2(polygon const&) = delete;
-    std::vector<line_string::cont_type> rings;
 
-    inline void add_ring(line_string && ring)
-    {
-        rings.emplace_back(std::move(ring.data));
-    }
+struct linear_ring : line_string {};
 
-    inline std::size_t num_rings() const
-    {
-        return rings.size();
-    }
-};
-
-//struct linear_ring : line_string {};
-using linear_ring = std::vector<point>;
 struct polygon3
 {
     linear_ring exterior_ring;
@@ -165,47 +121,8 @@ struct polygon3
 struct multi_point : std::vector<point> {};
 struct multi_line_string : std::vector<line_string> {};
 struct multi_polygon : std::vector<polygon3> {};
-
-struct polygon : vertex_sequence
-{
-    typedef line_string::cont_type::const_iterator iterator_type;
-    std::vector<std::tuple<std::uint32_t, std::uint32_t> > rings;
-    // ring's element count. first ring exterior, subsequent rings are interior
-    // rings[0] + ..+ rings[rings.size()-1] == data.size()
-    polygon() = default;
-    polygon (polygon && other) noexcept = default;
-    inline void add_ring(line_string && ring)
-    {
-        std::size_t count = ring.data.size();
-        if (count != 0)
-        {
-            std::size_t start = data.size();
-            data.resize(start + ring.data.size());
-            std::move_backward(ring.begin(),ring.end(), data.end());
-            rings.emplace_back(start,count);
-        }
-    }
-
-    inline std::size_t num_rings() const
-    {
-        return rings.size();
-    }
-
-    inline std::pair<iterator_type,iterator_type> ring(std::size_t index) const
-    {
-        if (index < num_rings())
-        {
-            std::tuple<std::uint32_t,std::uint32_t> const& ring = rings[index];
-            return std::make_pair(data.begin() + std::get<0>(ring), data.begin() + std::get<0>(ring) + std::get<1>(ring));
-        }
-        else
-        {
-            return std::make_pair(data.end(),data.end());
-        }
-    }
-};
-
 struct geometry_collection;
+
 typedef mapnik::util::variant< point,line_string, polygon3, multi_point, multi_line_string, multi_polygon, mapnik::util::recursive_wrapper<geometry_collection> > geometry;
 
 struct geometry_collection : std::vector<geometry> {};
@@ -241,14 +158,14 @@ struct line_string_vertex_adapter
     line_string_vertex_adapter(line_string const& line)
         : line_(line),
           current_index_(0),
-          end_index_(line.data.size())
+          end_index_(line.size())
     {}
 
     unsigned vertex(double*x, double*y) const
     {
         if (current_index_ != end_index_)
         {
-            point const& coord = line_.data[current_index_++];
+            point const& coord = line_[current_index_++];
             *x = coord.x;
             *y = coord.y;
             if (current_index_ == 1)
@@ -271,112 +188,6 @@ struct line_string_vertex_adapter
     mutable std::size_t current_index_;
     const std::size_t end_index_;
 
-};
-
-struct polygon_vertex_adapter
-{
-    polygon_vertex_adapter(polygon const& poly)
-        : poly_(poly),
-          rings_itr_(poly_.rings.begin()),
-          rings_end_(poly_.rings.end()),
-          current_index_(0),
-          end_index_(0),
-          start_loop_(false) {}
-
-    void rewind(unsigned) const
-    {
-        rings_itr_ = poly_.rings.begin();
-        current_index_ = 0;
-        end_index_ = 0;
-        start_loop_ = false;
-    }
-
-    unsigned vertex(double*x, double*y) const
-    {
-        if (current_index_ == end_index_)
-        {
-            if (rings_itr_ == rings_end_)
-                return mapnik::SEG_END;
-            current_index_ = std::get<0>(*rings_itr_);
-            end_index_ = current_index_ + std::get<1>(*rings_itr_);
-            ++rings_itr_;
-            start_loop_ = true;
-        }
-        point const& coord = poly_.data[current_index_++];
-        *x = coord.x;
-        *y = coord.y;
-
-        if (start_loop_)
-        {
-            start_loop_= false;
-            return mapnik::SEG_MOVETO;
-        }
-        if (current_index_ == end_index_)
-            return mapnik::SEG_CLOSE;
-        return mapnik::SEG_LINETO;
-    }
-private:
-    polygon const& poly_;
-    mutable std::vector<std::tuple<uint32_t,uint32_t> >::const_iterator rings_itr_;
-    mutable std::vector<std::tuple<uint32_t,uint32_t> >::const_iterator rings_end_;
-    mutable std::size_t current_index_;
-    mutable std::size_t end_index_;
-    mutable bool start_loop_;
-};
-
-struct polygon_vertex_adapter_2
-{
-    polygon_vertex_adapter_2(polygon2 const& poly)
-        : poly_(poly),
-          rings_itr_(0),
-          rings_end_(poly_.rings.size()),
-          current_index_(0),
-          end_index_((rings_itr_ < rings_end_) ? poly_.rings[0].size() : 0),
-          start_loop_(true) {}
-
-    void rewind(unsigned) const
-    {
-        rings_itr_ = 0;
-        rings_end_ = poly_.rings.size();
-        current_index_ = 0;
-        end_index_ = (rings_itr_ < rings_end_) ? poly_.rings[0].size() : 0;
-        start_loop_ = true;
-    }
-
-    unsigned vertex(double*x, double*y) const
-    {
-        if (rings_itr_ == rings_end_)
-            return mapnik::SEG_END;
-        if (current_index_ < end_index_)
-        {
-            point const& coord = poly_.rings[rings_itr_][current_index_++];
-            *x = coord.x;
-            *y = coord.y;
-            if (start_loop_)
-            {
-                start_loop_= false;
-                return mapnik::SEG_MOVETO;
-            }
-            return mapnik::SEG_LINETO;
-        }
-        else if (++rings_itr_ != rings_end_)
-        {
-            current_index_ = 0;
-            end_index_ = poly_.rings[rings_itr_].size();
-            point const& coord = poly_.rings[rings_itr_][current_index_++];
-            *x = coord.x;
-            *y = coord.y;
-            return mapnik::SEG_MOVETO;
-        }
-        return mapnik::SEG_END;
-    }
-private:
-    polygon2 const& poly_;
-    mutable std::size_t rings_itr_;
-    mutable std::size_t rings_end_;
-    mutable std::size_t current_index_;
-    mutable std::size_t end_index_;
-    mutable bool start_loop_;
 };
 
 struct polygon_vertex_adapter_3
@@ -454,19 +265,6 @@ struct vertex_processor
         -> typename std::result_of<processor_type(line_string_vertex_adapter const&)>::type
     {
         line_string_vertex_adapter va(line);
-        return proc_(va);
-    }
-    auto operator() (polygon const& poly) const
-        -> typename std::result_of<processor_type(polygon_vertex_adapter const&)>::type
-    {
-        polygon_vertex_adapter va(poly);
-        return proc_(va);
-    }
-
-    auto operator() (polygon2 const& poly) const
-        -> typename std::result_of<processor_type(polygon_vertex_adapter_2 const&)>::type
-    {
-        polygon_vertex_adapter_2 va(poly);
         return proc_(va);
     }
 
