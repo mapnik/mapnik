2818ff96e8156f5d2167f7b6203b38a70826a529
Dane Springmeyer <dane@mapbox.com>
2015-02-23 23:52:54 -0800

    test reversed point geojson

--- tests/cxx/geometry.cpp
+++ tests/cxx/geometry.cpp
@@ -11,28 +11,39 @@
 
 TEST_CASE("geometry") {
 
-SECTION("json") {
+SECTION("json point") {
     mapnik::util::file input("./tests/data/json/fixtures/point1.json");
     auto json = input.data();
     mapnik::new_geometry::geometry geom;
     std::string json_string(json.get());
     REQUIRE( mapnik::json::from_geojson(json_string,geom) );
-    if (geom.is<mapnik::new_geometry::point>()) {
-        auto const& point = mapnik::util::get<mapnik::new_geometry::point>(geom);
-        REQUIRE( point.x == 30 );
-        REQUIRE( point.y == 10 );
-        using adapter_type = mapnik::new_geometry::point_vertex_adapter;
-        adapter_type va(point);
-        std::string new_json;
-        using sink_type = std::back_insert_iterator<std::string>;
-        // TODO: need to round trip, but does not compile yet
-        /*
-        static const mapnik::json::geometry_generator_grammar<sink_type, adapter_type> grammar;
-        sink_type sink(new_json);
-        REQUIRE( boost::spirit::karma::generate(sink, grammar, va) );
-        */
-        //REQUIRE( mapnik::util::to_geojson(new_json,va) );
-    }
+    REQUIRE( geom.is<mapnik::new_geometry::point>() );
+    auto const& point = mapnik::util::get<mapnik::new_geometry::point>(geom);
+    REQUIRE( point.x == 30 );
+    REQUIRE( point.y == 10 );
+    using adapter_type = mapnik::new_geometry::point_vertex_adapter;
+    adapter_type va(point);
+    std::string new_json;
+    using sink_type = std::back_insert_iterator<std::string>;
+    // TODO: need to round trip, but does not compile yet
+    /*
+    static const mapnik::json::geometry_generator_grammar<sink_type, adapter_type> grammar;
+    sink_type sink(new_json);
+    REQUIRE( boost::spirit::karma::generate(sink, grammar, va) );
+    */
+    //REQUIRE( mapnik::util::to_geojson(new_json,va) );
+}
+
+SECTION("json point reversed") {
+    mapnik::util::file input("./tests/data/json/fixtures/point2.json");
+    auto json = input.data();
+    mapnik::new_geometry::geometry geom;
+    std::string json_string(json.get());
+    REQUIRE( mapnik::json::from_geojson(json_string,geom) );
+    REQUIRE( geom.is<mapnik::new_geometry::point>() );
+    auto const& point = mapnik::util::get<mapnik::new_geometry::point>(geom);
+    REQUIRE( point.x == 30 );
+    REQUIRE( point.y == 10 );
 }
 
 }
