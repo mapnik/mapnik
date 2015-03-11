c8e56f11289155c75355825611518d22a0d33ae0
Dane Springmeyer <dane@mapbox.com>
2015-03-10 18:15:52 -0700

    fix high zoom crash in line pattern rendering - closes #2726

--- src/agg/process_line_pattern_symbolizer.cpp
+++ src/agg/process_line_pattern_symbolizer.cpp
@@ -106,6 +106,7 @@ struct agg_renderer_process_visitor_l
         pattern_source source(image, opacity);
         pattern_type pattern (filter,source);
         renderer_type ren(ren_base, pattern);
+        ren.clip_box(0,0,common_.width_,common_.height_);
         rasterizer_type ras(ren);
 
         agg::trans_affine tr;
@@ -177,6 +178,7 @@ struct agg_renderer_process_visitor_l
         pattern_source source(image, opacity);
         pattern_type pattern (filter,source);
         renderer_type ren(ren_base, pattern);
+        ren.clip_box(0,0,common_.width_,common_.height_);
         rasterizer_type ras(ren);
 
         agg::trans_affine tr;
