067946bb587d3e88923738f9c8a4becb9e1f68e2
Blake Thompson <flippmoke@gmail.com>
2015-04-30 12:39:23 -0500

    Updated mason_latest again

--- mason_latest.sh
+++ mason_latest.sh
@@ -14,7 +14,10 @@ function mason_compile {
     HERE=$(pwd)
     make install
     if [[ `uname` == 'Darwin' ]]; then
-        install_name_tool -id @loader_path/libmapnik.dylib ${MASON_PREFIX}"/lib/libmapnik.dylib";     
+        install_name_tool -id @loader_path/libmapnik.dylib ${MASON_PREFIX}"/lib/libmapnik.dylib";
+        for f in ${MASON_PREFIX}"/lib/mapnik/input/*.input"; do
+            install_name_tool -id @loader_pather/input/`basename $f` $f
+        done;
     fi;
     python -c "data=open('$MASON_PREFIX/bin/mapnik-config','r').read();open('$MASON_PREFIX/bin/mapnik-config','w').write(data.replace('$HERE','.'))"
     mkdir -p ${MASON_PREFIX}/share/gdal
