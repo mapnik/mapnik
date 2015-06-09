11e58275e91f61c3a8a381cb6707ed80b1499dea
artemp <artem@mapnik.org>
2015-06-09 15:22:01 +0100

    fix typedef's

--- include/mapnik/csv/csv_grammar.hpp
+++ include/mapnik/csv/csv_grammar.hpp
@@ -20,11 +20,10 @@
  *
  *****************************************************************************/
 
-
-//#define BOOST_SPIRIT_DEBUG
-
 #ifndef MAPNIK_CVS_GRAMMAR_HPP
 #define MAPNIK_CVS_GRAMMAR_HPP
+
+//#define BOOST_SPIRIT_DEBUG
 
 #include <boost/spirit/include/qi.hpp>
 
@@ -37,38 +36,44 @@ using csv_line = columns;
 using csv_data = std::vector<csv_line>;
 
 template <typename Iterator>
-struct csv_line_grammar : qi::grammar<Iterator, csv_line(char), qi::blank_type>
+struct csv_line_grammar : qi::grammar<Iterator, csv_line(std::string const&), qi::blank_type>
 {
     csv_line_grammar() : csv_line_grammar::base_type(line)
     {
         using namespace qi;
+        qi::_r1_type _r1;
+        qi::lit_type lit;
+        qi::eol_type eol;
+        qi::char_type char_;
         line   = column(_r1) % char_(_r1)
             ;
-        column = quoted | *(char_ - (lit(_r1) | eol))
+        column = quoted | *(char_ - (lit(_r1) /*| eol*/))
             ;
-        quoted = '"' >> *("\"\"" | ~char_('"')) >> '"'
+        quoted = '"' >> *("\"\"" |  ~char_('"')) >> '"'
             ;
         //http://stackoverflow.com/questions/7436481/how-to-make-my-split-work-only-on-one-real-line-and-be-capable-to-skeep-quoted-p/7462539#7462539
         BOOST_SPIRIT_DEBUG_NODES((line)(column)(quoted));
     }
   private:
-    qi::rule<Iterator, csv_line(char), qi::blank_type> line;
-    qi::rule<Iterator, column(char)> column; // no-skip
+    qi::rule<Iterator, csv_line(std::string const&), qi::blank_type> line;
+    qi::rule<Iterator, column(std::string const&)> column; // no-skip
     qi::rule<Iterator, std::string()> quoted;
 };
 
 template <typename Iterator>
-struct csv_file_grammar : qi::grammar<Iterator, csv_data(char), qi::blank_type>
+struct csv_file_grammar : qi::grammar<Iterator, csv_data(std::string const&), qi::blank_type>
 {
     csv_file_grammar() : csv_file_grammar::base_type(start)
     {
         using namespace qi;
+        qi::eol_type eol;
+        qi::_r1_type _r1;
         start  = -line(_r1) % eol
             ;
         BOOST_SPIRIT_DEBUG_NODES((start));
     }
   private:
-    qi::rule<Iterator, csv_data(char), qi::blank_type> start;
+    qi::rule<Iterator, csv_data(std::string const&), qi::blank_type> start;
     csv_line_grammar<Iterator> line;
 };
 
