a5594bd44572a60e0a47ff09003b944ba44ce42e
Dane Springmeyer <dane@mapbox.com>
2015-07-02 09:57:12 -0700

    fix scan-build 'Value stored to 'a_' is never read' warning

--- deps/agg/include/agg_color_gray.h
+++ deps/agg/include/agg_color_gray.h
@@ -652,8 +652,8 @@ struct gray16
     //--------------------------------------------------------------------
     self_type& opacity(double a_)
     {
-        if (a_ < 0) a_ = 0;
-        else if(a_ > 1) a_ = 1;
+        if (a_ < 0) a = 0;
+        else if(a_ > 1) a = 1;
         else a = (value_type)uround(a_ * double(base_mask));
         return *this;
     }
