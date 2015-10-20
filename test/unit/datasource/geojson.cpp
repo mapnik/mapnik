2c72833629f7bdc97fdbb510e8a029e154259bc5
Dane Springmeyer <dane@mapbox.com>
2015-10-20 12:19:06 -0700

    Revert "travis - std::system() calls fails"
    This reverts commit 5de3a3776d6681113a1dcfd631b132adbbf68dc0.

--- test/unit/datasource/geojson.cpp
+++ test/unit/datasource/geojson.cpp
@@ -210,6 +210,7 @@ TEST_CASE("geojson") {
                 if (create_index)
                 {
                     detail::create_disk_index(filename, true);
+                    CHECK(mapnik::util::exists(filename + ".index"));
                 }
 
                 mapnik::parameters params;
