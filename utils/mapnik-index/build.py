3bb0f1748f7732ea503fb0cd8126f388059b3236
Dane Springmeyer <dane@mapbox.com>
2015-10-20 12:15:23 -0700

    icu linking for mapnik-index should be unneeded

--- utils/mapnik-index/build.py
+++ utils/mapnik-index/build.py
@@ -41,7 +41,6 @@ headers = env['CPPPATH']
 boost_program_options = 'boost_program_options%s' % env['BOOST_APPEND']
 boost_system = 'boost_system%s' % env['BOOST_APPEND']
 libraries =  [env['MAPNIK_NAME'], boost_program_options, boost_system]
-libraries.append(env['ICU_LIB_NAME'])
 libraries.append('mapnik-json')
 libraries.append('mapnik-wkt')
 
