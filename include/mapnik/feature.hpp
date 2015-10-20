bfbc72c4846309ecaa78ff917b7d31fa1a7201c2
artemp <artem@mapnik.org>
2015-10-20 14:36:19 +0100

    feature : put back set_id() for upstream compatibility

--- include/mapnik/feature.hpp
+++ include/mapnik/feature.hpp
@@ -109,7 +109,7 @@ public:
         raster_() {}
 
     inline mapnik::value_integer id() const { return id_;}
-
+    inline void set_id(mapnik::value_integer id) { id_ = id;}
     template <typename T>
     inline void put(context_type::key_type const& key, T const& val)
     {
@@ -257,7 +257,7 @@ public:
     }
 
 private:
-    const mapnik::value_integer id_;
+    mapnik::value_integer id_;
     context_ptr ctx_;
     cont_type data_;
     geometry::geometry<double> geom_;
