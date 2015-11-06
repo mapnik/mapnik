60fc5910f3a84329c0cc0a5909aa6a37cd1a0d8c
artemp <artem@mapnik.org>
2015-11-06 10:56:45 +0000

    mapnik-index - fix std::clog message typo

--- utils/mapnik-index/process_geojson_file.cpp
+++ utils/mapnik-index/process_geojson_file.cpp
@@ -88,7 +88,7 @@ std::pair<bool,box2d<double>> process_geojson_file(T & boxes, std::string const&
     {
         if (!boost::spirit::qi::phrase_parse(start, end, (geojson_datasource_static_bbox_grammar)(boost::phoenix::ref(boxes)) , space))
         {
-            std::clog << "mapnik-index (GeoJSON) : could extract bounding boxes from : '" <<  filename <<  "'";
+            std::clog << "mapnik-index (GeoJSON) : could not extract bounding boxes from : '" <<  filename <<  "'" << std::endl;
             return std::make_pair(false, extent);
         }
     }
