1a0c1ffe09f14b9bf3ae47130349dadea27bba1a
Daniel García Aubert <dgaubert@cartodb.com>
2015-11-10 14:52:20 +0100

    Back to previous whitespacing format

--- plugins/input/postgis/postgis_utils.cpp
+++ plugins/input/postgis/postgis_utils.cpp
@@ -202,28 +202,28 @@ private:
     	int nShift = 0;
     	uint8_t nByte;
 
-        /* Check so we don't read beyond the twkb */
-        while ( pos_ < size_ )
-        {
-            nByte = twkb_[pos_];
+    	/* Check so we don't read beyond the twkb */
+    	while ( pos_ < size_ )
+    	{
+    		nByte = twkb_[pos_];
 
-            /* We get here when there is more to read in the input varInt */
-            /* Here we take the least significant 7 bits of the read */
-            /* byte and put it in the most significant place in the result variable. */
-            nVal |= ((uint64_t)(nByte & 0x7f)) << nShift;
+    		/* We get here when there is more to read in the input varInt */
+    		/* Here we take the least significant 7 bits of the read */
+    		/* byte and put it in the most significant place in the result variable. */
+    		nVal |= ((uint64_t)(nByte & 0x7f)) << nShift;
 
-            /* move the "cursor" of the input buffer step (8 bits) */
-            pos_++;
+    		/* move the "cursor" of the input buffer step (8 bits) */
+    		pos_++;
 
-            /* move the cursor in the resulting variable (7 bits) */
-            nShift += 7;
+    		/* move the cursor in the resulting variable (7 bits) */
+    		nShift += 7;
 
-            /* Hibit isn't set, so this is the last byte */
-            if ( !(nByte & 0x80) )
-            {
-                return nVal;
-            }
-        }
+    		/* Hibit isn't set, so this is the last byte */
+    		if ( !(nByte & 0x80) )
+    		{
+    			return nVal;
+    		}
+    	}
                 
         // lwerror("%s: varint extends past end of buffer", __func__);
     	return 0;
