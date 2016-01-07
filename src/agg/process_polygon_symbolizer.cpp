c355c8843ef0b606a31e5e95b7bf9345d0ba3064
Paul Ramsey <pramsey@cleverelephant.ca>
2016-01-07 09:26:08 -0800

    Fill polygons irrespective of winding order (closes #11)

--- src/agg/process_polygon_symbolizer.cpp
+++ src/agg/process_polygon_symbolizer.cpp
@@ -98,6 +98,7 @@ void agg_renderer<T>::process(polygon_symbolizer const& sym,
     renderer_type ren(renb);
     ren.color(agg::rgba8_pre(r, g, b, int(a * sym.get_opacity())));
     agg::scanline_u8 sl;
+	ras_ptr->filling_rule(agg::fill_even_odd);
     agg::render_scanlines(*ras_ptr, sl, ren);
 
 }
