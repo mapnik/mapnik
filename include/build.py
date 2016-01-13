5804df758d1773ac4e3cfebf78be409efa1d4151
Mickey Rose <mickey.rose@seznam.cz>
2016-01-13 17:50:42 +0100

    throttle parallel compilation of specific sources

--- include/build.py
+++ include/build.py
@@ -55,3 +55,53 @@ if 'install' in COMMAND_LINE_TARGETS:
         env.Alias(target='install', source=env.Install(inc_target, includes))
 
 env['create_uninstall_target'](env, os.path.normpath(env['INSTALL_PREFIX']+'/include/mapnik/'))
+
+# Mark all `*_grammar.hpp` and `*_grammar_impl.hpp` headers as memory hogs.
+# Sources that #include (directly or indirectly) any of those are restricted
+# to at most --hogs=M parallel compilation tasks. They still count towards
+# the total limit given by --jobs=N, but don't prevent parallel compilation
+# of regular sources. There may M memory-intensive plus N-M memory-savvy
+# sources being compiled in parallel.
+env.MemoryHog(Split(
+    """
+    mapnik/css_color_grammar.hpp
+    mapnik/css_color_grammar_impl.hpp
+    mapnik/csv/csv_grammar.hpp
+    mapnik/expression_grammar.hpp
+    mapnik/expression_grammar_impl.hpp
+    mapnik/image_filter_grammar.hpp
+    mapnik/image_filter_grammar_impl.hpp
+    mapnik/json/extract_bounding_box_grammar.hpp
+    mapnik/json/extract_bounding_box_grammar_impl.hpp
+    mapnik/json/feature_collection_grammar.hpp
+    mapnik/json/feature_collection_grammar_impl.hpp
+    mapnik/json/feature_generator_grammar.hpp
+    mapnik/json/feature_generator_grammar_impl.hpp
+    mapnik/json/feature_grammar.hpp
+    mapnik/json/feature_grammar_impl.hpp
+    mapnik/json/geometry_generator_grammar.hpp
+    mapnik/json/geometry_generator_grammar_impl.hpp
+    mapnik/json/geometry_grammar.hpp
+    mapnik/json/geometry_grammar_impl.hpp
+    mapnik/json/positions_grammar.hpp
+    mapnik/json/positions_grammar_impl.hpp
+    mapnik/json/properties_generator_grammar.hpp
+    mapnik/json/properties_generator_grammar_impl.hpp
+    mapnik/json/symbolizer_grammar.hpp
+    mapnik/json/topojson_grammar.hpp
+    mapnik/json/topojson_grammar_impl.hpp
+    mapnik/path_expression_grammar.hpp
+    mapnik/path_expression_grammar_impl.hpp
+    mapnik/svg/output/svg_output_grammars.hpp
+    mapnik/svg/output/svg_output_grammars_impl.hpp
+    mapnik/svg/svg_path_grammar.hpp
+    mapnik/svg/svg_points_grammar.hpp
+    mapnik/svg/svg_transform_grammar.hpp
+    mapnik/transform_expression_grammar.hpp
+    mapnik/transform_expression_grammar_impl.hpp
+    mapnik/wkt/wkt_generator_grammar.hpp
+    mapnik/wkt/wkt_generator_grammar_impl.hpp
+    mapnik/wkt/wkt_grammar.hpp
+    mapnik/wkt/wkt_grammar_impl.hpp
+    """
+    ))
