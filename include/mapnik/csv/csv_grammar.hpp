da61043dd9ce17bb5105d54be5f29d79a0006a87
artemp <artem@mapnik.org>
2016-03-09 12:24:12 +0100

    split csv_line_grammar into *.hpp *_impl.hpp  ref #3343

--- include/mapnik/csv/csv_grammar.hpp
+++ include/mapnik/csv/csv_grammar.hpp
@@ -65,36 +65,7 @@ struct csv_white_space_skipper : qi::primitive_parser<csv_white_space_skipper>
 template <typename Iterator, typename Skipper = csv_white_space_skipper>
 struct csv_line_grammar : qi::grammar<Iterator, csv_line(char, char), Skipper>
 {
-    csv_line_grammar()
-        : csv_line_grammar::base_type(line)
-    {
-        qi::_r1_type _r1;
-        qi::_r2_type _r2;
-        qi::lit_type lit;
-        qi::char_type char_;
-        unesc_char.add
-            ("\\a", '\a')
-            ("\\b", '\b')
-            ("\\f", '\f')
-            ("\\n", '\n')
-            ("\\r", '\r')
-            ("\\t", '\t')
-            ("\\v", '\v')
-            ("\\\\",'\\')
-            ("\\\'", '\'')
-            ("\\\"", '\"')
-            ("\"\"", '\"') // double quote
-            ;
-        line = -lit("\r") > -lit("\n") > column(_r1, _r2) % lit(_r1)
-            ;
-        column = quoted(_r2) | *(char_ - lit(_r1))
-            ;
-        quoted = lit(_r1) > text(_r1) > lit(_r1) // support unmatched quotes or not (??)
-            ;
-        text = *(unesc_char | (char_ - lit(_r1)))
-            ;
-        BOOST_SPIRIT_DEBUG_NODES((line)(column)(quoted));
-    }
+    csv_line_grammar();
 private:
     qi::rule<Iterator, csv_line(char, char),  Skipper> line;
     qi::rule<Iterator, csv_value(char, char)> column; // no-skip
