bb0565d4d8620cc14db9ae39fef8c3907e35785b
Dane Springmeyer <dane@mapbox.com>
2016-03-10 16:25:54 -0800

    pin mason to avoid breakage from upcoming improvements - refs mapbox/mason#131

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,12 +10,15 @@ todo
 - shrink icu data
 '
 
+MASON_VERSION="694d08c"
+
 function setup_mason() {
     if [[ ! -d ./.mason ]]; then
-        git clone --depth 1 https://github.com/mapbox/mason.git ./.mason
+        git clone https://github.com/mapbox/mason.git ./.mason
+        git checkout ${MASON_VERSION}
     else
         echo "Updating to latest mason"
-        (cd ./.mason && git pull)
+        (cd ./.mason && git fetch && git checkout ${MASON_VERSION})
     fi
     export MASON_DIR=$(pwd)/.mason
     export PATH=$(pwd)/.mason:$PATH
