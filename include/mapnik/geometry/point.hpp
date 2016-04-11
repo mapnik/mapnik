0f711c0863578a2a4c8a97116d953add7e535c84
artemp <artem@mapnik.org>
2016-04-11 12:23:11 +0200

    geometry - move point/line_string/polygon into separate headers

---
+++ include/mapnik/geometry/point.hpp
@@ -0,0 +1,54 @@
+/*****************************************************************************
+ *
+ * This file is part of Mapnik (c++ mapping toolkit)
+ *
+ * Copyright (C) 2016 Artem Pavlenko
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ *****************************************************************************/
+
+#ifndef MAPNIK_GEOMETRY_POINT_HPP
+#define MAPNIK_GEOMETRY_POINT_HPP
+
+namespace mapnik { namespace geometry {
+
+template <typename T>
+struct point
+{
+    using value_type = T;
+    point() {}
+    point(T x_, T y_)
+        : x(x_), y(y_)
+    {}
+    value_type x;
+    value_type y;
+};
+
+template <typename T>
+bool operator==(point<T> const& lhs, point<T> const& rhs)
+{
+    return lhs.x == rhs.x && lhs.y == rhs.y;
+}
+
+template <typename T>
+bool operator!=(point<T> const& lhs, point<T> const& rhs)
+{
+    return lhs.x != rhs.x || lhs.y != rhs.y;
+}
+
+}}
+
+#endif // MAPNIK_GEOMETRY_POINT_HPP
