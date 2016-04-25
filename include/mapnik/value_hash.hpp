4b3b62a40ceb886f0f8fe686a83d17d3de7d19a3
artemp <artem@mapnik.org>
2016-04-25 20:43:32 +0200

    minor formatting

--- include/mapnik/value_hash.hpp
+++ include/mapnik/value_hash.hpp
@@ -41,7 +41,7 @@ template <class T>
 inline void hash_combine(std::size_t & seed, T const& v)
 {
     std::hash<T> hasher;
-    seed ^= hasher(v) + 0x9e3779b9 + (seed<<6) + (seed>>2);
+    seed ^= hasher(v) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
 }
 
 struct value_hasher
