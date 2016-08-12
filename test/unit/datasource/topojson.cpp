5c11fe49f9d428d140f73d513d43cebed4678a81
artemp <artem@mapnik.org>
2016-08-12 11:05:50 +0100

    geoson+topojson tests - correct expected test data (ref #3491)

--- test/unit/datasource/topojson.cpp
+++ test/unit/datasource/topojson.cpp
@@ -48,7 +48,6 @@ bool parse_topology(std::string const& filename, mapnik::topojson::topology & to
     return (result && (itr == end));
 }
 
-
 }
 
 TEST_CASE("topojson")
@@ -97,12 +96,12 @@ TEST_CASE("topojson")
                 attr{"description", tr.transcode("Test: \u005C")},
                 attr{"double", mapnik::value_double(1.1)},
                 attr{"int", mapnik::value_integer(1)},
-                attr{"object", tr.transcode("{name:\"waka\",spaces:\"value with spaces\",int:1,double:1.1,boolean:false"
-                                                            ",NOM_FR:\"Québec\",array:[\"string\",\"value with spaces\",3,1.1,null,true"
-                                                            ",\"Québec\"],another_object:{name:\"nested object\"}}")},
+                attr{"object", tr.transcode("{\"name\":\"waka\",\"spaces\":\"value with spaces\",\"int\":1,\"double\":1.1,\"boolean\":false"
+                                                            ",\"NOM_FR\":\"Québec\",\"array\":[\"string\",\"value with spaces\",3,1.1,null,true"
+                                                            ",\"Québec\"],\"another_object\":{\"name\":\"nested object\"}}")},
                 attr{"spaces", tr.transcode("this has spaces")},
                 attr{"array", tr.transcode("[\"string\",\"value with spaces\",3,1.1,null,true,"
-                                                           "\"Québec\",{name:\"object within an array\"},"
+                                                           "\"Québec\",{\"name\":\"object within an array\"},"
                                                            "[\"array\",\"within\",\"an\",\"array\"]]")},
                 attr{"empty_array", tr.transcode("[]")},
                 attr{"empty_object", tr.transcode("{}")},
