713b9e77888a6c71c7ef216ded842becc8decba3
artemp <artem@mapnik.org>
2017-03-02 15:09:11 +0100

    take #2 - try using pair instead of tuple

--- test/unit/text/shaping.cpp
+++ test/unit/text/shaping.cpp
@@ -7,7 +7,7 @@
 namespace {
 
 void test_shaping( mapnik::font_set const& fontset, mapnik::face_manager& fm,
-                   std::vector<std::tuple<unsigned, unsigned>> const& expected, char const* str, bool debug = false)
+                   std::vector<std::pair<unsigned, unsigned>> const& expected, char const* str, bool debug = false)
 {
     mapnik::transcoder tr("utf8");
     std::map<unsigned,double> width_map;
@@ -65,39 +65,39 @@ TEST_CASE("shaping")
     mapnik::face_manager fm(fl, font_file_mapping, font_memory_cache);
 
     {
-        std::vector<std::tuple<unsigned, unsigned>> expected =
-            {{{0, 0}, {0, 3}, {0, 4}, {0, 7}, {3, 8}, {11, 9}, {68, 10}, {69, 11}, {70, 12}, {12, 13}}};
+        std::vector<std::pair<unsigned, unsigned>> expected =
+            {{0, 0}, {0, 3}, {0, 4}, {0, 7}, {3, 8}, {11, 9}, {68, 10}, {69, 11}, {70, 12}, {12, 13}};
         // with default NotoSans-Regular.ttc and NotoNaskhArabic-Regular.ttf ^^^
-        //std::vector<std::tuple<unsigned, unsigned>> expected =
+        //std::vector<std::pair<unsigned, unsigned>> expected =
         //  {{977,0}, {1094,3}, {1038,4}, {1168,4}, {9,7}, {3,8}, {11,9}, {68,10}, {69,11}, {70,12}, {12,13}};
         // expected results if "NotoSansTibetan-Regular.ttf is registered^^
         test_shaping(fontset, fm, expected, u8"སྤུ་ཧྲེང (abc)");
     }
 
     {
-        std::vector<std::tuple<unsigned, unsigned>> expected =
-            {{{0, 0}, {0, 3}, {0, 4}, {0, 7}, {3, 8}, {11, 9}, {0, 10}, {0, 11}, {0, 12}, {12, 13}}};
+        std::vector<std::pair<unsigned, unsigned>> expected =
+            {{0, 0}, {0, 3}, {0, 4}, {0, 7}, {3, 8}, {11, 9}, {0, 10}, {0, 11}, {0, 12}, {12, 13}};
         test_shaping(fontset, fm, expected, u8"སྤུ་ཧྲེང (普兰镇)");
     }
 
     {
-        std::vector<std::tuple<unsigned, unsigned>> expected =
-            {{{68, 0}, {69, 1}, {70, 2}, {3, 3}, {11, 4}, {0, 5}, {0, 6}, {0, 7}, {12, 8}}};
+        std::vector<std::pair<unsigned, unsigned>> expected =
+            {{68, 0}, {69, 1}, {70, 2}, {3, 3}, {11, 4}, {0, 5}, {0, 6}, {0, 7}, {12, 8}};
         test_shaping(fontset, fm, expected, u8"abc (普兰镇)");
     }
 
     {
-        std::vector<std::tuple<unsigned, unsigned>> expected =
-            {{{68, 0}, {69, 1}, {70, 2}, {3, 3}, {11, 4}, {68, 5}, {69, 6}, {70, 7}, {12, 8}}};
+        std::vector<std::pair<unsigned, unsigned>> expected =
+            {{68, 0}, {69, 1}, {70, 2}, {3, 3}, {11, 4}, {68, 5}, {69, 6}, {70, 7}, {12, 8}};
         test_shaping(fontset, fm, expected, "abc (abc)");
     }
 
     {
         //    "ⵃⴰⵢ ⵚⵉⵏⴰⵄⵉ الحي الصناعي"
-        std::vector<std::tuple<unsigned, unsigned>> expected =
-            {{{0, 0}, {0, 1}, {0, 2}, {3, 3}, {0, 4}, {0, 5}, {0, 6}, {0, 7},
+        std::vector<std::pair<unsigned, unsigned>> expected =
+            {{0, 0}, {0, 1}, {0, 2}, {3, 3}, {0, 4}, {0, 5}, {0, 6}, {0, 7},
               {0, 8}, {0, 9}, {3, 10}, {509, 22}, {481, 21}, {438, 20}, {503, 19},
-              {470, 18}, {496, 17}, {43, 16}, {3, 15}, {509, 14}, {454, 13}, {496, 12}, {43, 11}}};
+              {470, 18}, {496, 17}, {43, 16}, {3, 15}, {509, 14}, {454, 13}, {496, 12}, {43, 11}};
         test_shaping(fontset, fm, expected, u8"ⵃⴰⵢ ⵚⵉⵏⴰⵄⵉ الحي الصناعي");
     }
 
