ef6c2e71b8f0af5a6b1c50151d5d612859ba2994
artemp <artem@mapnik.org>
2017-05-25 16:57:15 +0200

    svg-parser : handle parsing failures via `error_handler` ( throw in strict mode and log+continue when strict==false)

--- include/mapnik/svg/svg_parser.hpp
+++ include/mapnik/svg/svg_parser.hpp
@@ -36,22 +36,46 @@
 
 namespace  mapnik { namespace svg {
 
-    class MAPNIK_DECL svg_parser : private util::noncopyable
+class svg_parser_error_handler
+{
+    using error_message_container = std::vector<std::string> ;
+public:
+    explicit svg_parser_error_handler(bool strict = false)
+        : strict_(strict) {}
+
+    void on_error(std::string const& msg)
     {
-        using error_message_container = std::vector<std::string> ;
-    public:
-        explicit svg_parser(svg_converter_type & path, bool strict = false);
-        ~svg_parser();
-        error_message_container const& error_messages() const;
-        bool parse(std::string const& filename);
-        bool parse_from_string(std::string const& svg);
-        svg_converter_type & path_;
-        bool is_defs_;
-        bool strict_;
-        std::map<std::string, gradient> gradient_map_;
-        std::pair<std::string, gradient> temporary_gradient_;
-        error_message_container error_messages_;
-    };
+        if (strict_) throw std::runtime_error(msg);
+        else error_messages_.push_back(msg);
+    }
+    error_message_container const& error_messages() const
+    {
+        return error_messages_;
+    }
+    bool strict() const { return strict_; }
+private:
+
+    error_message_container error_messages_;
+    bool strict_;
+};
+
+class MAPNIK_DECL svg_parser : private util::noncopyable
+{
+    using error_handler = svg_parser_error_handler;
+public:
+    explicit svg_parser(svg_converter_type & path, bool strict = false);
+    ~svg_parser();
+    error_handler & err_handler();
+    bool parse(std::string const& filename);
+    bool parse_from_string(std::string const& svg);
+    svg_converter_type & path_;
+    bool is_defs_;
+    bool strict_;
+    std::map<std::string, gradient> gradient_map_;
+    std::pair<std::string, gradient> temporary_gradient_;
+    error_handler err_handler_;
+
+};
 
 }}
 
