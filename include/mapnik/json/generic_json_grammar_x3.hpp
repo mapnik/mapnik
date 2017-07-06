0ad1c580094c9ecbe95ea71c1c5bb8c735d853a6
artemp <artem@mapnik.org>
2017-07-06 11:11:01 +0200

    trying to `inline` lambdas (VS2017) (!)

--- include/mapnik/json/generic_json_grammar_x3.hpp
+++ include/mapnik/json/generic_json_grammar_x3.hpp
@@ -43,17 +43,17 @@ BOOST_SPIRIT_DECLARE(generic_json_grammar_type);
 BOOST_SPIRIT_DECLARE(generic_json_key_value_type);
 }
 
-auto assign = [](auto const& ctx)
+inline auto assign = [](auto const& ctx)
 {
     _val(ctx) = _attr(ctx);
 };
 
-auto assign_key = [](auto const& ctx)
+inline auto assign_key = [](auto const& ctx)
 {
-    std::get<0>(_val(ctx)) = _attr(ctx);
+    std::get<0>(_val(ctx)) = std::move(_attr(ctx));
 };
 
-auto assign_value = [](auto const& ctx)
+inline auto assign_value = [](auto const& ctx)
 {
     std::get<1>(_val(ctx)) = std::move(_attr(ctx));
 };
