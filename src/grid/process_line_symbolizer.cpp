d410c8c4371668d16d423c9dc56b64337577c04f
Raul Marin <git@rmr.ninja>
2018-05-25 17:12:05 +0200

    Improve padding calculation

--- src/grid/process_line_symbolizer.cpp
+++ src/grid/process_line_symbolizer.cpp
@@ -84,13 +84,11 @@ void grid_renderer<T>::process(line_symbolizer const& sym,
 
     if (clip)
     {
-        double padding = (double)(common_.query_extent_.width()/pixmap_.width());
-        double half_stroke = width/2.0;
-        if (half_stroke > 1)
-            padding *= half_stroke;
-        if (std::fabs(offset) > 0)
-            padding *= std::fabs(offset) * 1.2;
-        padding *= common_.scale_factor_;
+        double pad_per_pixel = static_cast<double>(common_.query_extent_.width()/common_.width_);
+        double pixels = std::ceil(std::max(width / 2.0 + std::fabs(offset),
+                                          (std::fabs(offset) * offset_converter_default_threshold)));
+        double padding = pad_per_pixel * pixels * common_.scale_factor_;
+
         clipping_extent.pad(padding);
     }
     using vertex_converter_type = vertex_converter<clip_line_tag, clip_poly_tag, transform_tag,
