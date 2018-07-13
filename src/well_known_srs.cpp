21d9d42b7f513f91e2913e1b9e9254a9682f56fd
Mickey Rose <mickey.rose@seznam.cz>
2018-07-13 16:30:32 +0200

    well_known_srs: move implementation to .cpp
    - add lonlat2merc, merc2lonlat overloads for single point
    - add stride argument to overloads for coordinate arrays
    - make all functions exported
    - change static string constants to exported
    - change static numeric constants to constexpr
    - remove superfluous numeric constants

--- src/well_known_srs.cpp
+++ src/well_known_srs.cpp
@@ -30,7 +30,17 @@
 #include <boost/optional.hpp>
 #pragma GCC diagnostic pop
 
+// stl
+#include <cmath>
+
 namespace mapnik {
+
+extern std::string const MAPNIK_LONGLAT_PROJ =
+    "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs";
+
+extern std::string const MAPNIK_GMERC_PROJ =
+    "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0"
+    " +y_0=0.0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs +over";
 
 static const char * well_known_srs_strings[] = {
     "mapnik-longlat",
@@ -81,5 +91,61 @@ boost::optional<bool> is_known_geographic(std::string const& srs)
 }
 
 IMPLEMENT_ENUM( well_known_srs_e, well_known_srs_strings )
+
+bool lonlat2merc(double & x, double & y)
+{
+    using namespace util;
+    auto dx = clamp(x, -180.0, 180.0);
+    auto dy = clamp(y, -MERC_MAX_LATITUDE, MERC_MAX_LATITUDE);
+    x = EARTH_RADIUS * radians(dx);
+    y = EARTH_RADIUS * std::log(std::tan(radians(90 + dy) / 2));
+    return true;
+}
+
+bool lonlat2merc(double * x, double * y, std::size_t point_count, std::size_t stride)
+{
+    for (std::size_t i = 0; i < point_count; ++i)
+    {
+        lonlat2merc(x[i * stride], y[i * stride]);
+    }
+    return true;
+}
+
+bool lonlat2merc(std::vector<geometry::point<double>> & ls)
+{
+    for (auto & p : ls)
+    {
+        lonlat2merc(p.x, p.y);
+    }
+    return true;
+}
+
+bool merc2lonlat(double & x, double & y)
+{
+    using namespace util;
+    auto rx = clamp(x / EARTH_RADIUS, -pi, pi);
+    auto ry = clamp(y / EARTH_RADIUS, -pi, pi);
+    x = degrees(rx);
+    y = degrees(2 * std::atan(std::exp(ry)) - pi / 2);
+    return true;
+}
+
+bool merc2lonlat(double * x, double * y, std::size_t point_count, std::size_t stride)
+{
+    for(std::size_t i = 0; i < point_count; ++i)
+    {
+        merc2lonlat(x[i * stride], y[i * stride]);
+    }
+    return true;
+}
+
+bool merc2lonlat(std::vector<geometry::point<double>> & ls)
+{
+    for (auto & p : ls)
+    {
+        merc2lonlat(p.x, p.y);
+    }
+    return true;
+}
 
 }
