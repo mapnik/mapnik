93c566638f99f90cc19e3bdb56c76ec6a10a0c08
Emil Gedda <emil.gedda@emilgedda.se>
2018-07-20 14:02:51 +0200

    remove old render_box function declaration from cairo_renderer.hpp

--- include/mapnik/cairo/cairo_renderer.hpp
+++ include/mapnik/cairo/cairo_renderer.hpp
@@ -171,7 +171,6 @@ public:
                        agg::trans_affine const& mtx,
                        double opacity=1.0,
                        bool recenter=true);
-    void render_box(box2d<double> const& b);
 protected:
     Map const& m_;
     cairo_context context_;
