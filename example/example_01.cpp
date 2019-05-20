bbbbe714c09d5521c78f04d9e791066ea19529e0
pvn <me@pedro-vicente.net>
2019-05-20 12:42:39 -0400

    build OSM plugin

--- example/example_01.cpp
+++ example/example_01.cpp
@@ -2,9 +2,18 @@
 #include <mapnik/load_map.hpp>
 #include <mapnik/agg_renderer.hpp>
 #include <mapnik/image_util.hpp>
+#include <mapnik/datasource_cache.hpp>
 
-int main()
+int main(int argc, char** argv)
 {
+  if (argc != 2)
+  {
+    std::cout << "usage: ./example_01 <OSM plugin location>\n";
+    return EXIT_SUCCESS;
+  }
+  std::string dir(argv[1]);
+  std::cout << " running example with OSM plugin in ..." << dir << "\n";
+  mapnik::datasource_cache::instance().register_datasources(dir);
   mapnik::Map m(256, 256);
   mapnik::load_map(m, "style_01.xml");
   m.zoom_all();
