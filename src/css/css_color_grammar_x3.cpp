2d65532f9d5e86f5d27ea2db84da47b9ce695d6b
Artem Pavlenko <artem@mapnik.org>
2020-02-04 11:40:52 +0000

    Use char const* as iterator type for CSS grammar

--- src/css/css_color_grammar_x3.cpp
+++ src/css/css_color_grammar_x3.cpp
@@ -29,11 +29,13 @@ namespace mapnik { namespace css_color_grammar {
 
 namespace x3 = boost::spirit::x3;
 using iterator_type = std::string::const_iterator;
+using iterator_css_type = char const*;
 using context_type = x3::phrase_parse_context<x3::ascii::space_type>::type;
-using context_skipper_type = x3::phrase_parse_context<css_grammar::css_skipper_type>::type;
+using context_css_type = x3::phrase_parse_context<css_grammar::css_skipper_type>::type;
+
 
 BOOST_SPIRIT_INSTANTIATE(css_color_grammar_type, iterator_type, context_type);
-BOOST_SPIRIT_INSTANTIATE(css_color_grammar_type, iterator_type, context_skipper_type);
+BOOST_SPIRIT_INSTANTIATE(css_color_grammar_type, iterator_css_type, context_css_type);
 
 #if BOOST_VERSION < 107000
 template bool parse_rule<iterator_type, context_type, mapnik::filter::color_to_alpha>
