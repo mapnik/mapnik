8f3dded09311ecdefe66c9c220f11a5ba1b18ad8
Artem Pavlenko <artem@mapnik.org>
2020-05-11 22:18:55 +0100

    Re-implement feature_json_generator by adapting feature_impl into boost::fusion container and removing use semantic actions (simpler code + boost_1_73 support) (ref #4143)

--- src/json/mapnik_feature_to_geojson.cpp
+++ src/json/mapnik_feature_to_geojson.cpp
@@ -32,7 +32,7 @@ namespace mapnik { namespace util {
 bool to_geojson(std::string & json, mapnik::feature_impl const& feature)
 {
     using sink_type = std::back_insert_iterator<std::string>;
-    static const mapnik::json::feature_generator_grammar<sink_type, mapnik::feature_impl> grammar;
+    static mapnik::json::feature_generator_grammar<sink_type, mapnik::feature_impl> const grammar;
     sink_type sink(json);
     return boost::spirit::karma::generate(sink, grammar, feature);
 }
