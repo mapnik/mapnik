b1a38feff7dbdc0dbec1a3f2deed487bf11138bd
Artem Pavlenko <artem@mapnik.org>
2021-03-18 16:05:09 +0000

    Add text to indicate if proj_transform is internal or external (libproj)

--- benchmark/src/test_proj_transform1.cpp
+++ benchmark/src/test_proj_transform1.cpp
@@ -66,9 +66,9 @@ int main(int argc, char** argv)
     std::string from_str2("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs");
     std::string to_str2("+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0.0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs +over");
     return benchmark::sequencer(argc, argv)
-        .run<test>("lonlat->merc epsg", from_str, to_str, from, to, true)
-        .run<test>("lonlat->merc literal", from_str2, to_str2, from, to, true)
-        .run<test>("merc->lonlat epsg", to_str, from_str, to, from, true)
-        .run<test>("merc->lonlat literal", to_str2, from_str2, to, from, true)
+        .run<test>("lonlat->merc epsg (internal)", from_str, to_str, from, to, true)
+        .run<test>("lonlat->merc literal (libproj)", from_str2, to_str2, from, to, true)
+        .run<test>("merc->lonlat epsg (internal)", to_str, from_str, to, from, true)
+        .run<test>("merc->lonlat literal (libproj)", to_str2, from_str2, to, from, true)
         .done();
 }
