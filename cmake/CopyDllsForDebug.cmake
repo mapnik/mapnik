bb800e6f5678edc3e765735130baeeb28d61d613
Mathis Logemann <mathisloge@gmail.com>
2021-04-09 15:50:50 +0200

    fixed Dll copying on windows; copy plugins and fonts into appropiate directories when building.

--- cmake/CopyDllsForDebug.cmake
+++ cmake/CopyDllsForDebug.cmake
@@ -11,19 +11,25 @@ if(RUN_IT)
 # Script ran by the add_custom_command
 	include(BundleUtilities)
 	include(InstallRequiredSystemLibraries)
+	string (REPLACE " " ";" TO_FIXUP_LIBS "${TO_FIXUP_LIBS}")
+	string (REPLACE " " ";" TO_FIXUP_DIRS "${TO_FIXUP_DIRS}")
+	#message(STATUS "${TO_FIXUP_FILE} ${TO_FIXUP_LIBS} ${TO_FIXUP_DIRS}")
 	fixup_bundle("${TO_FIXUP_FILE}" "${TO_FIXUP_LIBS}" "${TO_FIXUP_DIRS}")
 # End of script ran by the add_custom_command
 else()
 
 set(THIS_FILE ${CMAKE_CURRENT_LIST_FILE})
-function(copy_dlls_for_debug _targets _libs _dirs)
+function(copy_dlls_for_debug)
+	set(options)
+	set(oneValueArgs)
+	set(multiValueArgs TARGETS LIBS DIRS)
+	cmake_parse_arguments(MAPNIK_COPY_DLLS "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 	if(WIN32)
-		foreach(_target ${_targets})
+		foreach(_target IN LISTS MAPNIK_COPY_DLLS_TARGETS)
 			add_custom_command(
 				TARGET ${_target} POST_BUILD
-				COMMAND ${CMAKE_COMMAND} -DRUN_IT:BOOL=ON -DTO_FIXUP_FILE=$<TARGET_FILE:${_target}> -DTO_FIXUP_LIBS=${_libs} -DTO_FIXUP_DIRS=${_dirs} -P ${THIS_FILE}
+				COMMAND ${CMAKE_COMMAND} ARGS -DRUN_IT:BOOL=ON -DTO_FIXUP_FILE="$<TARGET_FILE:${_target}>" -DTO_FIXUP_LIBS:STRING="${MAPNIK_COPY_DLLS_LIBS}" -DTO_FIXUP_DIRS="${MAPNIK_COPY_DLLS_DIRS}" -P "${THIS_FILE}"
 				COMMENT "Fixing up dependencies for ${_target}"
-				VERBATIM
 			)
 		endforeach()
 	endif(WIN32)
