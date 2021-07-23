692da9dc00cb3e2a0758db18ec8e27b358d01682
Mathis Logemann <mathisloge@gmail.com>
2021-07-23 18:14:37 +0200

    delete clang toolchain(replaced by CMakePresets)

--- cmake/clang.cmake
+++
@@ -1,6 +0,0 @@
-set(CMAKE_C_COMPILER clang)
-set(CMAKE_CXX_COMPILER clang++)
-
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libc++")
-set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -stdlib=libc++")
-set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -stdlib=libc++")
