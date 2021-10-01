333e745d7bfbfb46502b42e3030ce29e26950734
Mathis Logemann <mathisloge@gmail.com>
2021-10-01 20:42:57 +0200

    [CMake] pkg-config: generate correct library link file names

--- cmake/MapnikExportPkgConfig.cmake
+++ cmake/MapnikExportPkgConfig.cmake
@@ -1,4 +1,4 @@
-function(create_pkg_config_file _lib_name _description)
+function(create_pkg_config_file _target _lib_name _description)
     string(CONFIGURE [[
 prefix=@CMAKE_INSTALL_PREFIX@
 exec_prefix=${prefix}
@@ -8,7 +8,7 @@ libdir=${exec_prefix}/lib
 Name: @_lib_name@
 Description: @_description@
 Version: @MAPNIK_VERSION@
-Libs: -L"${libdir}" -l@_lib_name@
+Libs: -L"${libdir}" -l$<TARGET_FILE_PREFIX:@_target@>$<TARGET_FILE_BASE_NAME:@_target@>$<TARGET_PROPERTY:@_target@,$<CONFIG>_POSTFIX>
 Cflags: -I"${includedir}" ]] 
     _contents @ONLY)
 
@@ -38,7 +38,7 @@ Name: @_lib_name@
 Description: @_description@
 Version: @MAPNIK_VERSION@
 Requires: libmapnikwkt libmapnikjson
-Libs: -L"${libdir}" -l@_lib_name@ -llibmapnikwkt -llibmapnikjson
+Libs: -L"${libdir}" -l$<TARGET_FILE_PREFIX:mapnik>$<TARGET_FILE_BASE_NAME:mapnik>$<TARGET_PROPERTY:mapnik,$<CONFIG>_POSTFIX> -l$<TARGET_FILE_PREFIX:json>$<TARGET_FILE_BASE_NAME:json>$<TARGET_PROPERTY:json,$<CONFIG>_POSTFIX> -l$<TARGET_FILE_PREFIX:wkt>$<TARGET_FILE_BASE_NAME:wkt>$<TARGET_PROPERTY:wkt,$<CONFIG>_POSTFIX>
 Cflags: -I"${includedir}" @_str_compile_defs@]] 
     _contents @ONLY)
     file(GENERATE
@@ -52,6 +52,6 @@ Cflags: -I"${includedir}" @_str_compile_defs@]]
 endfunction()
 
 
-create_pkg_config_file(libmapnikwkt "wkt library")
-create_pkg_config_file(libmapnikjson "json library")
+create_pkg_config_file(wkt libmapnikwkt "wkt library")
+create_pkg_config_file(json libmapnikjson "json library")
 create_pkg_config_file_mapnik("libmapnik" "mapnik library")
