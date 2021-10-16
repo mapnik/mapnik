5ea441a7fe79b69100e2fa0e3eae3f33182c9c21
Mathis Logemann <mathisloge@gmail.com>
2021-10-16 08:31:19 +0200

    [CMake] fix mapnik_option

--- cmake/MapnikOption.cmake
+++ cmake/MapnikOption.cmake
@@ -1,4 +1,4 @@
 macro(mapnik_option option_name option_description option_default_value)
     option(${option_name} "${option_description}" ${option_default_value})
-    add_feature_info(${option_name} ${${option_name}} "${option_description}")
+    add_feature_info(${option_name} ${option_name} "${option_description}")
 endmacro()
