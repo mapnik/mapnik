78e03e1c77ff8198668f78f5c3c49ce8b94af17f
Mathis Logemann <mathisloge@gmail.com>
2022-01-26 23:32:21 +0100

    format dir benchmark

--- benchmark/src/test_numeric_cast_vs_static_cast.cpp
+++ benchmark/src/test_numeric_cast_vs_static_cast.cpp
@@ -9,23 +9,25 @@ class test_static : public benchmark::test_case
 {
     double step_;
     std::uint8_t start_;
-public:
+
+  public:
     test_static(mapnik::parameters const& params)
-     : test_case(params),
-       step_(STEP_NUM),
-       start_(START_NUM) {}
-    bool validate() const
-    {
-        return true;
-    }
+        : test_case(params)
+        , step_(STEP_NUM)
+        , start_(START_NUM)
+    {}
+    bool validate() const { return true; }
     bool operator()() const
     {
         double value_ = 0.0;
         std::uint8_t x;
-        for (std::size_t i=0;i<iterations_;++i) {
+        for (std::size_t i = 0; i < iterations_; ++i)
+        {
             double c = static_cast<double>(start_) * value_;
-            if (c >= 256.0) c = 255.0;
-            if (c < 0.0) c = 0.0;
+            if (c >= 256.0)
+                c = 255.0;
+            if (c < 0.0)
+                c = 0.0;
             x = static_cast<std::uint8_t>(c);
             value_ += step_;
         }
@@ -33,35 +35,34 @@ public:
     }
 };
 
-using boost::numeric::positive_overflow;
 using boost::numeric::negative_overflow;
+using boost::numeric::positive_overflow;
 
 class test_numeric : public benchmark::test_case
 {
     double step_;
     std::uint8_t start_;
-public:
+
+  public:
     test_numeric(mapnik::parameters const& params)
-     : test_case(params),
-       step_(STEP_NUM),
-       start_(START_NUM) {}
-    bool validate() const
-    {
-        return true;
-    }
+        : test_case(params)
+        , step_(STEP_NUM)
+        , start_(START_NUM)
+    {}
+    bool validate() const { return true; }
     bool operator()() const
     {
         double value_ = 0.0;
         std::uint8_t x;
-        for (std::size_t i=0;i<iterations_;++i) {
-            try {
+        for (std::size_t i = 0; i < iterations_; ++i)
+        {
+            try
+            {
                 x = boost::numeric_cast<std::uint8_t>(start_ * value_);
-            }
-            catch(negative_overflow&)
+            } catch (negative_overflow&)
             {
                 x = std::numeric_limits<std::uint8_t>::min();
-            }
-            catch(positive_overflow&)
+            } catch (positive_overflow&)
             {
                 x = std::numeric_limits<std::uint8_t>::max();
             }
@@ -73,8 +74,5 @@ public:
 
 int main(int argc, char** argv)
 {
-    return benchmark::sequencer(argc, argv)
-        .run<test_static>("static_cast")
-        .run<test_numeric>("numeric_cast")
-        .done();
+    return benchmark::sequencer(argc, argv).run<test_static>("static_cast").run<test_numeric>("numeric_cast").done();
 }
--- benchmark/src/test_proj_transform1.cpp
+++ benchmark/src/test_proj_transform1.cpp
@@ -10,45 +10,46 @@ class test : public benchmark::test_case
     mapnik::box2d<double> from_;
     mapnik::box2d<double> to_;
     bool defer_proj_init_;
-public:
+
+  public:
     test(mapnik::parameters const& params,
          std::string const& src,
          std::string const& dest,
          mapnik::box2d<double> const& from,
          mapnik::box2d<double> const& to,
          bool defer_proj)
-     : test_case(params),
-       src_(src),
-       dest_(dest),
-       from_(from),
-       to_(to),
-       defer_proj_init_(defer_proj) {}
+        : test_case(params)
+        , src_(src)
+        , dest_(dest)
+        , from_(from)
+        , to_(to)
+        , defer_proj_init_(defer_proj)
+    {}
     bool validate() const
     {
-        mapnik::projection src(src_,defer_proj_init_);
-        mapnik::projection dest(dest_,defer_proj_init_);
-        mapnik::proj_transform tr(src,dest);
+        mapnik::projection src(src_, defer_proj_init_);
+        mapnik::projection dest(dest_, defer_proj_init_);
+        mapnik::proj_transform tr(src, dest);
         mapnik::box2d<double> bbox = from_;
-        if (!tr.forward(bbox)) return false;
-        return ((std::fabs(bbox.minx() - to_.minx()) < .5) &&
-                (std::fabs(bbox.maxx() - to_.maxx()) < .5) &&
-                (std::fabs(bbox.miny() - to_.miny()) < .5) &&
-                (std::fabs(bbox.maxy() - to_.maxy()) < .5)
-               );
+        if (!tr.forward(bbox))
+            return false;
+        return ((std::fabs(bbox.minx() - to_.minx()) < .5) && (std::fabs(bbox.maxx() - to_.maxx()) < .5) &&
+                (std::fabs(bbox.miny() - to_.miny()) < .5) && (std::fabs(bbox.maxy() - to_.maxy()) < .5));
     }
     bool operator()() const
     {
-        mapnik::projection src(src_,defer_proj_init_);
-        mapnik::projection dest(dest_,defer_proj_init_);
-        mapnik::proj_transform tr(src,dest);
-        for (std::size_t i=0;i<iterations_;++i)
+        mapnik::projection src(src_, defer_proj_init_);
+        mapnik::projection dest(dest_, defer_proj_init_);
+        mapnik::proj_transform tr(src, dest);
+        for (std::size_t i = 0; i < iterations_; ++i)
         {
-            for (int j=-180;j<180;j=j+5)
+            for (int j = -180; j < 180; j = j + 5)
             {
-                for (int k=-85;k<85;k=k+5)
+                for (int k = -85; k < 85; k = k + 5)
                 {
-                    mapnik::box2d<double> box(j,k,j,k);
-                    if (!tr.forward(box)) throw std::runtime_error("could not transform coords");
+                    mapnik::box2d<double> box(j, k, j, k);
+                    if (!tr.forward(box))
+                        throw std::runtime_error("could not transform coords");
                 }
             }
         }
@@ -59,16 +60,17 @@ public:
 // echo -180 -60 | cs2cs -f "%.10f" epsg:4326 +to epsg:3857
 int main(int argc, char** argv)
 {
-    mapnik::box2d<double> from(-180,-80,180,80);
-    mapnik::box2d<double> to(-20037508.3427892476,-15538711.0963092316,20037508.3427892476,15538711.0963092316);
+    mapnik::box2d<double> from(-180, -80, 180, 80);
+    mapnik::box2d<double> to(-20037508.3427892476, -15538711.0963092316, 20037508.3427892476, 15538711.0963092316);
     std::string from_str("epsg:4326");
     std::string to_str("epsg:3857");
     std::string from_str2("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs");
-    std::string to_str2("+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0.0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs +over");
+    std::string to_str2("+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0.0 +k=1.0 +units=m "
+                        "+nadgrids=@null +wktext +no_defs +over");
     return benchmark::sequencer(argc, argv)
-        .run<test>("lonlat->merc epsg (internal)", from_str, to_str, from, to, true)
-        .run<test>("lonlat->merc literal (libproj)", from_str2, to_str2, from, to, true)
-        .run<test>("merc->lonlat epsg (internal)", to_str, from_str, to, from, true)
-        .run<test>("merc->lonlat literal (libproj)", to_str2, from_str2, to, from, true)
-        .done();
+      .run<test>("lonlat->merc epsg (internal)", from_str, to_str, from, to, true)
+      .run<test>("lonlat->merc literal (libproj)", from_str2, to_str2, from, to, true)
+      .run<test>("merc->lonlat epsg (internal)", to_str, from_str, to, from, true)
+      .run<test>("merc->lonlat literal (libproj)", to_str2, from_str2, to, from, true)
+      .done();
 }
