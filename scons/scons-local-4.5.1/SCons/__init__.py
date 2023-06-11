ed0f8dcb48c331e1b456793132d0a97517921965
Artem Pavlenko <artem@mapnik.org>
2023-06-11 11:31:23 +0100

    Add missing scons-local-4.5.1

---
+++ scons/scons-local-4.5.1/SCons/__init__.py
@@ -0,0 +1,9 @@
+__version__="4.5.1"
+__copyright__="Copyright (c) 2001 - 2023 The SCons Foundation"
+__developer__="bdbaddog"
+__date__="Mon, 06 Mar 2023 23:32:38 -0400"
+__buildsys__="M1DOG2021"
+__revision__="49578b34582d9e92dac7d713a8e58599ae35aa63"
+__build__="49578b34582d9e92dac7d713a8e58599ae35aa63"
+# make sure compatibility is always in place
+import SCons.compat # noqa
