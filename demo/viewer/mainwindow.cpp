1c6d14eb85e5a93d4f20ade2bf9dda81c8f84bf1
Artem Pavlenko <artem@mapnik.org>
2024-02-09 09:19:26 +0000

    viewer.app - Increase max scale factor to 10

--- demo/viewer/mainwindow.cpp
+++ demo/viewer/mainwindow.cpp
@@ -387,7 +387,7 @@ void MainWindow::createToolBars()
 
     scale_factor_ = new QDoubleSpinBox(fileToolBar);
     scale_factor_->setMinimum(0.1);
-    scale_factor_->setMaximum(5.0);
+    scale_factor_->setMaximum(10.0);
     scale_factor_->setSingleStep(0.1);
     scale_factor_->setValue(1.0);
 
