f4012c5a528f12d36aa1a7a941d8d903eeb5d317
leedejun <lidejun152046@126.com>
2024-04-23 14:29:16 +0800

    修改错误
    修改错误

--- demo/viewer/cehuidatainfo.hpp
+++ demo/viewer/cehuidatainfo.hpp
@@ -12,7 +12,7 @@ struct cehuidataInfo
     std::string NAME="";
     std::string Type="";
     std::string DIRECTION="";   //单向 双向
-    std::string WIDTH = "10"
+    std::string WIDTH = "10";
     std::string LEVEL="3";   //道路等级，1县道，2省道，3国道，4高速
     std::string geometryWkt="";
 
@@ -23,7 +23,7 @@ struct cehuidataInfo
         Type="";
         DIRECTION="";   //单向 双向
         WIDTH = "10";
-        LEVEL="3";   //道路等级，1县道，2省道，3国道，4高速
+        LEVEL= "3";   //道路等级，1县道，2省道，3国道，4高速
         geometryWkt="";
     }
 };
