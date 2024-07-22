fc029ae5a5dcbf0491bd8ac540442c0f8cce0bc4
Artem Pavlenko <artem@mapnik.org>
2024-07-22 10:20:47 +0100

    Update Coopyright year

--- demo/viewer/mapwidget.cpp
+++ demo/viewer/mapwidget.cpp
@@ -1,6 +1,6 @@
 /* This file is part of Mapnik (c++ mapping toolkit)
  *
- * Copyright (C) 2021 Artem Pavlenko
+ * Copyright (C) 2024 Artem Pavlenko
  *
  * Mapnik is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
--- plugins/input/ogr/ogr_utils.hpp
+++ plugins/input/ogr/ogr_utils.hpp
@@ -2,7 +2,7 @@
  *
  * This file is part of Mapnik (c++ mapping toolkit)
  *
- * Copyright (C) 2023 Artem Pavlenko
+ * Copyright (C) 2024 Artem Pavlenko
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
